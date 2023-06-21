34a4df9850fe31cbfcdb49dba3b2f3000f02368f
hdx <hdx@dcloud.io>
2023-06-21 16:14:03 +0800

    feat: add slider switch

--- App.vue
+++ App.vue
@@ -1,19 +1,24 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+  export default {
+    onLaunch: function() {
+      console.log('App Launch')
+
+      // const performance: Performance = uni.getPerformance()
+      // const observer1: PerformanceObserver = performance.createObserver((entryList: PerformanceObserverEntryList) => {
+      //   console.log("observer1:entryList.getEntries()" + JSON.stringify(entryList.getEntries()))
+      // })
+      // observer1.observe({ entryTypes: ['render', 'navigation'] } as PerformanceObserverOptions)
+    },
+    onShow: function() {
+      console.log('App Show')
+    },
+    onHide: function() {
+      console.log('App Hide')
+    }
+  }
 </script>
 
 <style>
-	/*每个页面公共css */
-    @import './common/uni.css';
-
+  /*每个页面公共css */
+  @import './common/uni.css';
 </style>
