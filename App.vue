d05bb5c224805a18c6ca2b845d80d4fae9dfb811
zhenyuWang <13641039885@163.com>
2023-07-05 15:31:03 +0800

    feat(lifecycle): onLastPageBackPress

--- App.vue
+++ App.vue
@@ -1,53 +1,57 @@
 <script>
-    export default {
-        onLaunch: function() {
-            console.log('App Launch')
+export default {
+  onLaunch: function () {
+    console.log("App Launch");
 
-            // const performance: Performance = uni.getPerformance()
-            // const observer1: PerformanceObserver = performance.createObserver((entryList: PerformanceObserverEntryList) => {
-            //   console.log("observer1:entryList.getEntries()" + JSON.stringify(entryList.getEntries()))
-            // })
-            // observer1.observe({ entryTypes: ['render', 'navigation'] } as PerformanceObserverOptions)
-        },
-        onShow: function() {
-            console.log('App Show')
-        },
-        onHide: function() {
-            console.log('App Hide')
-        }
-    }
+    // const performance: Performance = uni.getPerformance()
+    // const observer1: PerformanceObserver = performance.createObserver((entryList: PerformanceObserverEntryList) => {
+    //   console.log("observer1:entryList.getEntries()" + JSON.stringify(entryList.getEntries()))
+    // })
+    // observer1.observe({ entryTypes: ['render', 'navigation'] } as PerformanceObserverOptions)
+  },
+  onShow: function () {
+    console.log("App Show");
+  },
+  onHide: function () {
+    console.log("App Hide");
+  },
+  onLastPageBackPress: function (): boolean | null {
+    console.log("App LastPageBackPress");
+    return null;
+  },
+};
 </script>
 
 <style>
-    /*每个页面公共css */
-    @import './common/uni.css';
+/*每个页面公共css */
+@import "./common/uni.css";
 
-    /* #ifdef H5 */
-    @media screen and (min-width: 768px) {
-        body {
-            overflow-y: scroll;
-        }
-    }
+/* #ifdef H5 */
+@media screen and (min-width: 768px) {
+  body {
+    overflow-y: scroll;
+  }
+}
 
-    /* 顶栏通栏样式 */
-    /* .uni-top-window {
+/* 顶栏通栏样式 */
+/* .uni-top-window {
   	    left: 0;
   	    right: 0;
   	} */
 
-    uni-page-body {
-        background-color: #F5F5F5 !important;
-        min-height: 100% !important;
-        height: auto !important;
-    }
+uni-page-body {
+  background-color: #f5f5f5 !important;
+  min-height: 100% !important;
+  height: auto !important;
+}
 
-    .uni-top-window uni-tabbar .uni-tabbar {
-        background-color: #fff !important;
-    }
+.uni-top-window uni-tabbar .uni-tabbar {
+  background-color: #fff !important;
+}
 
-    .uni-app--showleftwindow .hideOnPc {
-        display: none !important;
-    }
+.uni-app--showleftwindow .hideOnPc {
+  display: none !important;
+}
 
-    /* #endif */
+/* #endif */
 </style>
