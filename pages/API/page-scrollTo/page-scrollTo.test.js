04977efd219985fc15c982df917a957c327a52d9
hdx <hdx@dcloud.io>
2023-07-06 15:01:07 +0800

    feat: add api pageScrollTo

---
+++ pages/API/page-scrollTo/page-scrollTo.test.js
@@ -0,0 +1,26 @@
+const PAGE_PATH = '/pages/API/page-scrollTo/page-scrollTo'
+
+describe('page-scrollTo', () => {
+  let page
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor(500)
+  })
+  // TODO
+  // it('scrollTo', async () => {
+  //   const btnScrollTo = await page.$('.btn-scrollTo')
+
+  //   await toggle.tap()
+  //   await page.waitFor(50)
+  //   expect(await page.scrollTop).toBe(100)
+  // })
+  // it('scrollToElement', async () => {
+  //   const btnScrollTo = await page.$('.btn-scrollToElement')
+  //   const scrollToElement = await page.$('.custom-element')
+
+  //   await btnScrollTo.tap()
+  //   await page.waitFor(50)
+  //   const offset = await scrollToElement.offset()
+  //   expect(offset.top).toBe(0)
+  // })
+})
