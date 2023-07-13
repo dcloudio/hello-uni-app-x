558dff520854d14a49bb7874b2a7521b726a4f13
zhenyuWang <13641039885@163.com>
2023-07-13 15:23:50 +0800

    fix: remove useless page & add annotation

--- App.vue
+++ App.vue
@@ -2,37 +2,48 @@
 import { state, setLifeCycleNum } from './store/index.uts'
 export default {
   onLaunch: function () {
+    // 自动化测试
     setLifeCycleNum(state.lifeCycleNum + 1000)
-    console.log("App Launch");
+    console.log('App Launch')
 
-    // const performance: Performance = uni.getPerformance()
-    // const observer1: PerformanceObserver = performance.createObserver((entryList: PerformanceObserverEntryList) => {
-    //   console.log("observer1:entryList.getEntries()" + JSON.stringify(entryList.getEntries()))
-    // })
-    // observer1.observe({ entryTypes: ['render', 'navigation'] } as PerformanceObserverOptions)
+    const performance: Performance = uni.getPerformance()
+    const observer1: PerformanceObserver = performance.createObserver(
+      (entryList: PerformanceObserverEntryList) => {
+        console.log(
+          'observer1:entryList.getEntries()' +
+            JSON.stringify(entryList.getEntries())
+        )
+      }
+    )
+    observer1.observe({
+      entryTypes: ['render', 'navigation'],
+    } as PerformanceObserverOptions)
   },
   onShow: function () {
+    // 自动化测试
     setLifeCycleNum(state.lifeCycleNum + 100)
-    console.log("App Show");
+    console.log('App Show')
   },
   onHide: function () {
+    // 自动化测试
     setLifeCycleNum(state.lifeCycleNum - 100)
-    console.log("App Hide");
+    console.log('App Hide')
   },
   onLastPageBackPress: function (): boolean | null {
+    // 自动化测试
     setLifeCycleNum(state.lifeCycleNum - 1000)
     uni.showToast({
-      title: "再按一次退出应用",
-      position: "bottom",
-    });
-    return null;
+      title: '再按一次退出应用',
+      position: 'bottom',
+    })
+    return null
   },
-};
+}
 </script>
 
 <style>
 /*每个页面公共css */
-@import "./common/uni.css";
+@import './common/uni.css';
 
 /* #ifdef H5 */
 @media screen and (min-width: 768px) {
