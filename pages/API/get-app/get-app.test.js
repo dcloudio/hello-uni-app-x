4b5ec57789a9225b28e6ec36006b12df3a32134b
zhenyuWang <13641039885@163.com>
2023-07-17 14:33:13 +0800

    feat: getApp

---
+++ pages/API/get-app/get-app.test.js
@@ -0,0 +1,11 @@
+const PAGE_PATH = '/pages/API/get-app/get-app'
+
+describe('getApp', () => {
+  it('getApp', async () => {
+    const page = await program.navigateTo(PAGE_PATH)
+    await page.waitFor(1000)
+    await page.callMethod('getApp')
+    const data = await page.data()
+    expect(data.checked).toBe(true)
+  })
+})
