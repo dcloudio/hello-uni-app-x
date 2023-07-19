dc8319920690ff8014a88b74db026722dfb872f3
zhenyuWang <13641039885@163.com>
2023-07-19 13:30:08 +0800

    feat: getCurrentPages

--- pages/API/get-current-pages/get-current-pages.test.js
+++ pages/API/get-current-pages/get-current-pages.test.js
@@ -1,11 +1,18 @@
 const PAGE_PATH = '/pages/API/get-current-pages/get-current-pages'
 
 describe('getCurrentPages', () => {
+	let page
   it('getCurrentPages', async () => {
-    const page = await program.navigateTo(PAGE_PATH)
+    page = await program.navigateTo(PAGE_PATH)
     await page.waitFor(1000)
-    await page.callMethod('getCurrentPages')
+    await page.callMethod('_getCurrentPages')
     const data = await page.data()
     expect(data.checked).toBe(true)
+  })
+  it('hideAndShow', async () => {
+    await page.callMethod('hideAndShow')
+    await page.waitFor(1000)
+    const data = await page.data()
+    expect(data.showTimes).toBe(2)
   })
 })
