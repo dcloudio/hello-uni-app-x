93bcf0923d517eac6789974abbdd68a31512b14e
zhenyuWang <13641039885@163.com>
2023-07-19 21:55:33 +0800

    wip(api): getApp

--- pages/API/get-app/get-app.test.js
+++ pages/API/get-app/get-app.test.js
@@ -4,7 +4,7 @@ describe('getApp', () => {
   it('getApp', async () => {
     const page = await program.navigateTo(PAGE_PATH)
     await page.waitFor(1000)
-    await page.callMethod('getApp')
+    await page.callMethod('_getApp')
     const data = await page.data()
     expect(data.checked).toBe(true)
   })
