d2d25f5427611d06eebcdaeb0b9211a0e66cc280
zhenyuWang <13641039885@163.com>
2023-08-30 16:11:09 +0800

    test: ref

--- pages/component/general-attribute/general-attribute.test.js
+++ pages/component/general-attribute/general-attribute.test.js
@@ -7,14 +7,20 @@ describe('general attribute', () => {
     page = await program.navigateTo(PAGE_PATH)
     await page.waitFor(1000)
   })
-  it('screenshot', async () => {
-    const image = await program.screenshot()
-    expect(image).toMatchImageSnapshot()
-  })
+  it("class & style", async () => {
+    const image = await program.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
   it('validateGeneralAttributes', async () => {
-    const button = await page.$('.btn')
+    const button = await page.$(".btn-style");
     await button.tap()
     const btnInner = await page.$('.btn-inner')
     expect(await btnInner.text()).toBe('基础属性验证成功')
   })
+  it("ref", async () => {
+    const button = await page.$(".btn-ref");
+    await button.tap();
+    const image = await program.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
 })
