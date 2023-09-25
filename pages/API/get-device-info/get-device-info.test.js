b8557693837a45d18db8d65ef4b875e3e99e12eb
taohebin <taohebin@dcloud.io>
2023-09-25 18:45:14 +0800

    update: 补充部分ext-api的自动化测试

---
+++ pages/API/get-device-info/get-device-info.test.js
@@ -0,0 +1,28 @@
+const PAGE_PATH = '/pages/API/get-device-info/get-device-info'
+
+describe('ExtApi-GetDeviceInfo', () => {
+
+  let page;
+  let res;
+  const stringProperties = [
+    'brand', 'deviceBrand', 'deviceId', 'model', 'deviceModel',
+    'deviceType', 'devicePixelRatio', 'system', 'platform', 'uniRuntimeVersion',
+  ]
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor(600);
+    res = await uni.getDeviceInfo();
+  });
+  it('Check properties', async () => {
+    for (const key in res) {
+      const value = res[key];
+      expect(value).not.toBeNull();
+      if (stringProperties.indexOf(key) != -1) {
+        expect(value).not.toBe("");
+      }
+      if (key == 'deviceOrientation') {
+        expect(['portrait', 'landscape']).toContain(value);
+      }
+    }
+  });
+});
