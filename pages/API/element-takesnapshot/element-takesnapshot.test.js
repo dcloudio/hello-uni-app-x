bff58493c26e02c7a34c1c966b599d7c7a4ac3aa
duqingquan <duqingquan@dcloud.io>
2023-10-18 17:06:24 +0800

    dix takesnapshot autotest 虚拟机测试失败的问题

--- pages/API/element-takesnapshot/element-takesnapshot.test.js
+++ pages/API/element-takesnapshot/element-takesnapshot.test.js
@@ -13,15 +13,15 @@ describe("element-takesnapshot", () => {
 
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
-    await page.waitFor(600);
+    await page.waitFor(1200);
   });
 
 
   it("takeSnapshot", async () => {
-    await page.waitFor(600)
+    await page.waitFor(1200)
     let btnTakeSnapshot = await page.$('.btn-TakeSnapshot')
     await btnTakeSnapshot.tap()
-    page.waitFor(600)
+    await page.waitFor(1200)
     const image = await getData('snapImage')
     console.log(image)
     ///storage/emulated/0/Android/data/io.dcloud.uniappx/apps/__UNI__3584C99/cache/temp/screenshot/1697513148915.png
