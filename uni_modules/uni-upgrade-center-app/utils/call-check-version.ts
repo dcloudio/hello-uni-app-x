ad080cdeefaf7a23b0f7c3eaab61cb45adeacbb7
DCloud_LXH <283700113@qq.com>
2023-11-02 19:51:16 +0800

    feat: 新增 升级中心

---
+++ uni_modules/uni-upgrade-center-app/utils/call-check-version.ts
@@ -0,0 +1,109 @@
+export type StoreListItem = {
+	enable : boolean
+	id : string
+	name : string
+	scheme : string
+	priority : number // 优先级
+}
+
+export type UniUpgradeCenterResult = {
+	_id : string
+	appid : string
+	name : string
+	title : string
+	contents : string
+	url : string // 安装包下载地址
+	platform : Array<string> // Array<'Android' | 'iOS'>
+	version : string // 版本号 1.0.0
+	uni_platform : string // "android" | "ios" // 版本号 1.0.0
+	stable_publish : boolean // 是否是稳定版
+	is_mandatory : boolean // 是否强制更新
+	is_silently : boolean	// 是否静默更新
+	create_env : string // "upgrade-center"
+	create_date : number
+	message : string
+	code : number
+
+	type : string // "native_app" | "wgt"
+	store_list : StoreListItem[] | null
+	min_uni_version : string | null  // 升级 wgt 的最低 uni-app 版本
+}
+
+export default function (): Promise<UniUpgradeCenterResult> {
+	// #ifdef APP-PLUS
+	return new Promise<UniUpgradeCenterResult>((resolve, reject) => {
+		const systemInfo = uni.getSystemInfoSync()
+		const appId = systemInfo.appId
+		const appVersion = systemInfo.appVersion
+		// #ifndef UNI-APP-X
+		if (plus.runtime.appid && plus.runtime.version) {
+			plus.runtime.getProperty(plus.runtime.appid, function (widgetInfo) {
+				if (widgetInfo.version) {
+					let data = {
+						action: 'checkVersion',
+						appid: plus.runtime.appid,
+						appVersion: plus.runtime.version,
+						wgtVersion: widgetInfo.version
+					}
+					uniCloud.callFunction({
+						name: 'uni-upgrade-center',
+						data: data,
+						success: (e) => {
+							resolve(e.result as UniUpgradeCenterResult)
+						},
+						fail: (error) => {
+							reject(error)
+						}
+					})
+				} else {
+					reject('widgetInfo.version is EMPTY')
+				}
+			})
+		} else {
+			reject('plus.runtime.appid is EMPTY')
+		}
+		// #endif
+		// // #ifdef UNI-APP-X
+		if (typeof appId === 'string' && typeof appVersion === 'string' && appId.length > 0 && appVersion.length > 0) {
+			let data = {
+				action: 'checkVersion',
+				appid: appId,
+				appVersion: appVersion,
+				wgtVersion: '0.0.0.0.0.1'
+			}
+			try {
+				uniCloud.callFunction({
+					name: 'uni-upgrade-center',
+					data: data
+				}).then(res => {
+					const result = JSON.parse<UniUpgradeCenterResult>(JSON.stringify(res.result))
+					if (result === null ){
+						reject({
+							code: res.result['code'],
+							message: res.result['message']
+						})
+					} else {
+						resolve(result)
+					}
+				}).catch<void>((err : any | null) => {
+					console.log('err: ', err);
+					const error = err as UniCloudError
+					reject(error.errMsg)
+				})
+			} catch (e) {
+				reject(e.message)
+			}
+		} else {
+			reject('plus.runtime.appid is EMPTY')
+		}
+		// #endif
+	})
+	// #endif
+	// #ifndef APP-PLUS
+	/* return new Promise((resolve, reject) => {
+		reject({
+			message: '请在App中使用'
+		})
+	}) */
+	// #endif
+}
