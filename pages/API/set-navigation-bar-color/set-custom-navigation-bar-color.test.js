a089887d0d764b6d0ab079759162c1a6681560b1
zhenyuWang <13641039885@163.com>
2023-11-07 13:42:23 +0800

    test:  set-custom-navigation-bar-color 截图测试适配不同分辨率

--- pages/API/set-navigation-bar-color/set-custom-navigation-bar-color.test.js
+++ pages/API/set-navigation-bar-color/set-custom-navigation-bar-color.test.js
@@ -4,8 +4,19 @@ const CURRENT_PAGE_PATH =
 describe("setCustomNavigationBarColor", () => {
   let page;
   let originLifeCycleNum;
+  const screenShotArea = { x: 880, y: 0, width: 100, height: 60 };
   beforeAll(async () => {
+    console.log('process.env.uniTestPlatformInfo', process.env.uniTestPlatformInfo )
     page = await program.navigateTo(CURRENT_PAGE_PATH);
+    if(process.env.uniTestPlatformInfo.startsWith('android 6')){
+      screenShotArea.x = 535
+      screenShotArea.width = 90
+      screenShotArea.height = 50
+    }else if(process.env.uniTestPlatformInfo.startsWith('android 12')){
+      screenShotArea.x = 1160
+      screenShotArea.width = 140
+      screenShotArea.height = 80
+    }
     await page.waitFor(1000);
     originLifeCycleNum = await page.callMethod("getLifeCycleNum");
   });
@@ -21,7 +32,7 @@ describe("setCustomNavigationBarColor", () => {
     await page.waitFor(1000);
     const image = await program.screenshot({
       adb: true,
-      area: { x: 880, y: 0, width: 100, height: 60 },
+      area: screenShotArea,
     });
     expect(image).toMatchImageSnapshot();
     const lifeCycleNum = await page.callMethod("getLifeCycleNum");
@@ -33,7 +44,7 @@ describe("setCustomNavigationBarColor", () => {
     await page.waitFor(1000);
     const image = await program.screenshot({
       adb: true,
-      area: { x: 880, y: 0, width: 100, height: 60 },
+      area: screenShotArea,
     });
     expect(image).toMatchImageSnapshot();
     const lifeCycleNum = await page.callMethod("getLifeCycleNum");
