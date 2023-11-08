52112ded9d9211c4bd8878784d484da30ea90829
shutao <shutao@dcloud.io>
2023-11-08 19:06:41 +0800

    add sticky-section.uvue

---
+++ pages/component/sticky-section/sticky-section.test.js
@@ -0,0 +1,14 @@
+describe('component-native-sticky-section', () => {
+  let page
+  beforeAll(async () => {
+    page = await program.reLaunch('/pages/component/sticky-section/sticky-section')
+    await page.waitFor('sticky-section')
+  })
+
+  //检测吸顶上推效果
+  it('check_sticky_section', async () => {
+    await page.callMethod('listViewScrollByY', 1000)
+    const image = await program.screenshot();
+    expect(image).toMatchImageSnapshot();
+  })
+})
