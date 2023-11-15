4aeed860fed03b7905277f17dca6ab1b4e01462b
shutao <shutao@dcloud.io>
2023-11-15 17:12:52 +0800

    添加 list-view-multiplex.nvue 测试list-item部分复用场景

---
+++ pages/component/list-view/list-view-multiplex.test.js
@@ -0,0 +1,16 @@
+describe('component-native-list-view', () => {
+  let page
+  beforeAll(async () => {
+    //打开list-view-multiplex测试页
+    page = await program.reLaunch('/pages/component/list-view/list-view-multiplex')
+    await page.waitFor('list-view')
+  })
+
+  //滚动list-view到底部 加载更多 如果异常则直接闪退
+  it('check_list_item_multiplex', async () => {
+    await page.callMethod('listViewScrollByY', 5000)
+    await page.waitFor(400)
+    await page.callMethod('listViewScrollByY', 100)
+  })
+
+})
