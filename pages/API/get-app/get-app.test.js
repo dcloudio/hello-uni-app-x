f4f4296210b5a5641578d50448df6a409df6b375
zhenyuWang <13641039885@163.com>
2023-11-21 19:31:12 +0800

    feat: 补充 globalData 示例及测试

--- pages/API/get-app/get-app.test.js
+++ pages/API/get-app/get-app.test.js
@@ -1,13 +1,23 @@
 const PAGE_PATH = '/pages/API/get-app/get-app'
 
 describe('getApp', () => {
-  it('getApp', async () => {
-    const page = await program.navigateTo(PAGE_PATH)
-    await page.waitFor(1000)
-    const oldData = await page.data()
+  let page = null
+  beforeAll(async () => {
+    page = await program.navigateTo(PAGE_PATH)
+    await page.waitFor('view')
+  })
+  it('globalData', async () => {
+    await page.callMethod('getGlobalData')
+    const data = await page.data()
+    expect(data.globalDataStr).toBe('global data str')
+    expect(data.globalDataNum).toBe(123)
+    expect(data.globalDataBool).toBe(true)
+  })
+  it('method', async () => {
+    const oldLifeCycleNum = await page.data('lifeCycleNum')
     await page.callMethod('_increasetLifeCycleNum')
-    const newData = await page.data()
-    expect(newData.lifeCycleNum - oldData.lifeCycleNum).toBe(100)
-    await page.callMethod('setLifeCycleNum', oldData.lifeCycleNum)
+    const newLifeCycleNum = await page.data('lifeCycleNum')
+    expect(newLifeCycleNum - oldLifeCycleNum).toBe(100)
+    await page.callMethod('setLifeCycleNum', oldLifeCycleNum)
   })
 })
