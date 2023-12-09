2055b54e012ba27e9ba1158eb333ca5e04f00cde
zhenyuWang <13641039885@163.com>
2023-12-09 23:01:22 +0800

    test: 修复错误冲突合并

--- pages/API/nodes-info/nodes-info.test.js
+++ pages/API/nodes-info/nodes-info.test.js
@@ -23,13 +23,6 @@ describe('nodes-info', () => {
     expect(Math.round(nodeInfo.left)).toBe(RECT_LEFT)
     expect(Math.round(nodeInfo.width)).toBe(RECT_WIDTH)
     expect(Math.round(nodeInfo.height)).toBe(RECT_HEIGHT)
-    // android 6 分辨率为 720*1280，需要调整期望数值
-    if (!process.env.uniTestPlatformInfo.startsWith('android 6')) {
-      const nodeInfo = data.nodeInfoList[0]
-      expect(nodeInfo.left > 15).toBe(true)
-      expect(nodeInfo.width == 150).toBe(true)
-      expect(nodeInfo.height == 100).toBe(true)
-    }
   })
   it('get-all-node-info', async () => {
     const btnGetAllNodeInfo = await page.$('.btn-get-all-node-info')
@@ -44,23 +37,11 @@ describe('nodes-info', () => {
     expect(nodeInfo1.top > 220).toBe(true)
     expect(Math.round(nodeInfo1.width)).toBe(RECT_WIDTH)
     expect(Math.round(nodeInfo1.height)).toBe(RECT_HEIGHT)
-    if (!process.env.uniTestPlatformInfo.startsWith('android 6')) {
-      const nodeInfo1 = data.nodeInfoList[0]
-      expect(nodeInfo1.left > 15).toBe(true)
-      expect(nodeInfo1.top > 220).toBe(true)
-      expect(nodeInfo1.width == 150).toBe(true)
-      expect(nodeInfo1.height == 100).toBe(true)
 
     const nodeInfo2 = data.nodeInfoList[1]
     expect(nodeInfo2.left > 200).toBe(true)
     expect(nodeInfo2.top > 220).toBe(true)
     expect(Math.round(nodeInfo2.width)).toBe(RECT_WIDTH)
     expect(Math.round(nodeInfo2.height)).toBe(RECT_HEIGHT)
-      const nodeInfo2 = data.nodeInfoList[1]
-      expect(nodeInfo2.left > 200).toBe(true)
-      expect(nodeInfo2.top > 220).toBe(true)
-      expect(nodeInfo2.width == 150).toBe(true)
-      expect(nodeInfo2.height == 100).toBe(true)
-    }
   })
 })
--- pages/component/progress/progress.test.js
+++ pages/component/progress/progress.test.js
@@ -7,8 +7,6 @@ function getData(key = '') {
 
 let page
 beforeAll(async () => {
-    page = await program.reLaunch('/pages/component/progress/progress')
-    await page.waitFor(2000);
   page = await program.reLaunch('/pages/component/progress/progress')
   await page.waitFor(2000);
 })
@@ -82,68 +80,4 @@ describe('Progress.uvue', () => {
     })
     expect(await el.property('background-color')).toEqual('#007aff')
   })
-    it('percent', async () => {
-        await page.callMethod('setProgress')
-        await page.waitFor(1000);
-        const p = await page.$('.p')
-        expect(await p.property('percent')).toEqual(20)
-        const p1 = await page.$('.p1')
-        expect(await p1.property('percent')).toEqual(40)
-        const p2 = await page.$('.p2')
-        expect(await p2.property('percent')).toEqual(60)
-        const p3 = await page.$('.p3')
-        expect(await p3.property('percent')).toEqual(80)
-        expect(await getData('curPercent')).toEqual(20)
-        await page.callMethod('clearProgress')
-        await page.waitFor(1000)
-        expect(await p.property('percent')).toEqual(0)
-        expect(await p1.property('percent')).toEqual(0)
-        expect(await p2.property('percent')).toEqual(0)
-        expect(await p3.property('percent')).toEqual(0)
-        expect(await getData('curPercent')).toEqual(0)
-    })
-    it('length', async () => {
-        const elements = await page.$$('.progress')
-        expect(elements.length).toBe(4)
-    })
-    it('show-info', async () => {
-        const el = await page.$('.p')
-        expect(await el.property('show-info')).toEqual(true)
-        await page.setData({
-            showInfo: false
-        })
-        expect(await el.property('show-info')).toEqual(false)
-    })
-    it('border-radius', async () => {
-        const el = await page.$('.p')
-        expect(await el.property('border-radius')).toEqual(0)
-        await page.setData({
-            borderRadius: 5
-        })
-        expect(await el.property('border-radius')).toEqual(5)
-    })
-    it('font-size', async () => {
-        const el = await page.$('.p')
-        expect(await el.property('font-size')).toEqual(16)
-        await page.setData({
-            fontSize: 18
-        })
-        expect(await el.property('font-size')).toEqual(18)
-    })
-    it('stroke-width', async () => {
-        const el = await page.$('.p')
-        expect(await el.property('stroke-width')).toEqual(3)
-        await page.setData({
-            strokeWidth: 6
-        })
-        expect(await el.property('stroke-width')).toEqual(6)
-    })
-    it('backgroundColor', async () => {
-        const el = await page.$('.p')
-        expect(await el.property('background-color')).toEqual('#EBEBEB')
-        await page.setData({
-            backgroundColor: "#007aff"
-        })
-        expect(await el.property('background-color')).toEqual('#007aff')
-    })
 })
