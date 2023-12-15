aac4ab89e91ce5e339f8ccfa6f2c9eebf1bc14cc
wangyaqi <wangyaqi988@163.com>
2023-12-15 20:31:24 +0800

    fix: 一键登录自动获取客户端appId

--- uniCloud-aliyun/cloudfunctions/univerify/index.js
+++ uniCloud-aliyun/cloudfunctions/univerify/index.js
@@ -1,15 +1,15 @@
 'use strict';
-exports.main = async function(event) {
+exports.main = async function(event, context) {
   const res = await uniCloud.getPhoneNumber({
-    appid: '__UNI__3584C99', // 替换成自己开通一键登录的应用的DCloud appid
+    appid: context.appId, // 替换成自己开通一键登录的应用的DCloud appid
     provider: 'univerify',
     access_token: event.access_token,
     openid: event.openid
   })
   // 执行入库等操作，正常情况下不要把完整手机号返回给前端
   return {
-    code: 0,
-    message: '获取手机号成功',
+    errCode: 0,
+    errMsg: '获取手机号成功',
     res: res
   }
 }
