2f781c49edd7dcac9ecff758a054164eff0c72ec
taohebin <taohebin@dcloud.io>
2024-01-02 18:44:33 +0800

    update: 补充上传下载的cookie测试例

--- pages/API/upload-file/upload-file.test.js
+++ pages/API/upload-file/upload-file.test.js
@@ -12,7 +12,26 @@ describe('ExtApi-UploadFile', () => {
     res = await page.data('jest_result');
   });
 
+  beforeEach(async () => {
+    await page.setData({
+      jest_result: false
+    })
+  });
+
   it('Check ', async () => {
     expect(res).toBe(true);
+  });
+
+  it('Check Set Cookie', async () => {
+    res = await page.callMethod('jest_set_cookie')
+    await page.waitFor(1000);
+    res = await page.data('jest_result');
+    expect(res).toBe(true)
+  });
+  it('Check Delete Cookie', async () => {
+    res = await page.callMethod('jest_delete_cookie')
+    await page.waitFor(1000);
+    res = await page.data('jest_result');
+    expect(res).toBe(true)
   });
 });
