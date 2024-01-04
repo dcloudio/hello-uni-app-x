ae92c8a1b006232b68a2834c88d4ade79a6f7a0d
wangyaqi <wangyaqi988@163.com>
2024-01-04 13:40:52 +0800

    feat: 调整示例与测试例适配web端

--- pages/API/request/request.test.js
+++ pages/API/request/request.test.js
@@ -79,16 +79,19 @@ describe('ExtApi-Request', () => {
     await request(page, 'HEAD');
   });
 
-  it('Check Set Cookie', async () => {
-    res = await page.callMethod('jest_set_cookie')
-    await page.waitFor(500);
-    res = await page.data('jest_result');
-    expect(res).toBe(true)
-  });
-  it('Check Delete Cookie', async () => {
-    res = await page.callMethod('jest_delete_cookie')
-    await page.waitFor(1000);
-    res = await page.data('jest_result');
-    expect(res).toBe(true)
-  });
+
+  if (process.env.uniTestPlatformInfo.indexOf('web') === -1) {
+    it('Check Set Cookie', async () => {
+      res = await page.callMethod('jest_set_cookie')
+      await page.waitFor(500);
+      res = await page.data('jest_result');
+      expect(res).toBe(true)
+    });
+    it('Check Delete Cookie', async () => {
+      res = await page.callMethod('jest_delete_cookie')
+      await page.waitFor(1000);
+      res = await page.data('jest_result');
+      expect(res).toBe(true)
+    });
+  }
 });
