e2451010e58c9669a6359f551c50c221d9eea399
zhenyuWang <13641039885@163.com>
2024-01-23 18:05:23 +0800

    test(scroll-view-refresher): 调整测试平台兼容性

--- pages/component/scroll-view/scroll-view-refresher.test.js
+++ pages/component/scroll-view/scroll-view-refresher.test.js
@@ -1,19 +1,24 @@
 // uni-app自动化测试教程: uni-app自动化测试教程: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/
 
 describe('test title', () => {
-
+  if (process.env.uniTestPlatformInfo.startsWith('android')) {
     let page;
     beforeAll(async () => {
-        page = await program.reLaunch('/pages/component/scroll-view/scroll-view-refresher');
-        await page.waitFor(300);
+      page = await program.reLaunch('/pages/component/scroll-view/scroll-view-refresher');
+      await page.waitFor(300);
     });
 
 
     it('check_refresher', async () => {
       await page.setData({
-          refresherTriggered: true
+        refresherTriggered: true
       })
       await page.waitFor(2000);
       expect(await page.data('refresherrefresh')).toBe(true)
     });
+  } else {
+    it('other platform', () => {
+      expect(1).toBe(1)
+    })
+  }
 });
