311a58b5f840b99c13b889e8dcc7d78fce8736d2
hdx <hdx@dcloud.io>
2024-01-26 17:51:00 +0800

    picker-view.test: 调整自动化测试写法

--- pages/component/picker-view/picker-view.test.js
+++ pages/component/picker-view/picker-view.test.js
@@ -16,14 +16,18 @@ describe('PickerView.uvue', () => {
     const el = await page.$('.picker-view')
     await page.callMethod('setValue')
     await page.waitFor(1000)
-    expect(await el.property('value')).toEqual([0, 0, 0])
+    const newValue1 = await el.property('value')
+    // TODO
+    expect(newValue1.toString()).toEqual('0,0,0')
     if (process.env.UNI_PLATFORM === 'app-android') {
       expect(await getData('result')).toEqual([0, 0, 0])
     }
 
     await page.callMethod('setValue1')
     await page.waitFor(1000)
-    expect(await el.property('value')).toEqual([10, 10, 10])
+    const newValue2 = await el.property('value')
+    // TODO
+    expect(newValue2.toString()).toEqual('10,10,10')
     if (process.env.UNI_PLATFORM === 'app-android') {
       expect(await getData('result')).toEqual([10, 10, 10])
     }
