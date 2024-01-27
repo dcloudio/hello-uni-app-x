7aa368feb8ac2e09f14464105ef640317400e92a
zhenyuWang <13641039885@163.com>
2024-01-27 09:03:32 +0800

    perf: 放开 getElementById 入口，并优化测试

--- pages/API/get-element-by-id/get-element-by-id.test.js
+++ pages/API/get-element-by-id/get-element-by-id.test.js
@@ -2,12 +2,6 @@ const PAGE_PATH = "/pages/API/get-element-by-id/get-element-by-id";
 let page;
 
 describe("getElementById", () => {
-  if (process.env.uniTestPlatformInfo.indexOf('web') > -1) {
-    it('dummyTest', () => {
-      expect(1).toBe(1)
-    })
-    return
-  }
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH);
     await page.waitFor('view');
