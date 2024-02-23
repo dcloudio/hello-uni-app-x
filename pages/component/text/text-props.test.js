1f59f1b399e3e96d5b749e675123bfbb51a2d200
yinjiacheng <yinjiacheng@dcloud.io>
2024-02-23 19:59:38 +0800

    更新text示例和自动化测试用例

--- pages/component/text/text-props.test.js
+++ pages/component/text/text-props.test.js
@@ -1,12 +1,23 @@
 const PAGE_PATH = '/pages/component/text/text-props'
 
 describe('text-props', () => {
+  let page
   beforeAll(async () => {
-    const page = await program.navigateTo(PAGE_PATH)
+    page = await program.navigateTo(PAGE_PATH)
     await page.waitFor(1000)
   })
+
   it('screenshot', async () => {
     const image = await program.screenshot({ fullPage: true })
     expect(image).toMatchImageSnapshot()
+  })
+
+  it('empty text', async () => {
+      const element = await page.$('#empty-text')
+      if (element != null) {
+        const { width, height } = await element.size()
+        expect(width).toBe(0)
+        expect(height).toBe(0)
+      }
   })
 })
