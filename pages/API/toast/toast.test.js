e6a42110fa80353c4e5a4475bc1309cf78e819f3
wangyaqi <wangyaqi988@163.com>
2024-03-01 20:00:17 +0800

    test: 延长toast hide等待时间

--- pages/API/toast/toast.test.js
+++ pages/API/toast/toast.test.js
@@ -109,7 +109,7 @@ describe('API-toast', () => {
 
     const btnToastHideButton = await page.$('#btn-toast-hide')
     await btnToastHideButton.tap()
-    await page.waitFor(200)
+    await page.waitFor(1000)
 
     if (isAndroid) {
       const image = await program.screenshot({
