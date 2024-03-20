69b71284932e7b7993f0bf4509b4b37f6004dee6
yinjiacheng <yinjiacheng@dcloud.io>
2024-03-20 16:07:31 +0800

    更新video自动化测试用例

--- pages/component/video/video.test.js
+++ pages/component/video/video.test.js
@@ -1,5 +1,3 @@
-// uni-app自动化测试教程: uni-app自动化测试教程: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/
-jest.setTimeout(20000);
 describe('component-native-video', () => {
   if(process.env.uniTestPlatformInfo.startsWith('web')){
     // TODO: web 端暂不支持测试
@@ -9,47 +7,25 @@ describe('component-native-video', () => {
     return
   }
   let page;
-
   beforeAll(async () => {
     page = await program.reLaunch('/pages/component/video/video');
-    await page.waitFor('video');
-    await page.waitFor(300)
+    await page.waitFor(1000);
   });
 
   it('test API', async () => {
+    expect(await page.data('isError')).toBe(false);
     // play
     await page.callMethod('play');
-    await page.waitFor(async () => {
-      return (await page.data('isPlaying')) == true;
-    });
-    // requestFullScreen
-    // await page.callMethod('requestFullScreen', null);
-    // await page.waitFor(async () => {
-    //   return (await page.data('isFullScreen')) == true;
-    // });
-    // exitFullScreen
-    // await page.callMethod('exitFullScreen');
-    // await page.waitFor(async () => {
-    //   return (await page.data('isFullScreen')) == false;
-    // });
+    await page.waitFor(100);
+    expect(await page.data('isPlaying')).toBe(true);
     // pause
     await page.callMethod('pause');
-    await page.waitFor(async () => {
-      return (await page.data('isPause')) == true;
-    });
-    // await page.callMethod('play');
-    // await page.waitFor(async () => {
-    //   return (await page.data('isPlaying')) == true;
-    // });
-    // stop
-    // await page.callMethod('stop');
-    // await page.waitFor(async () => {
-    //   return (await page.data('isPause')) == true;
-    // });
+    await page.waitFor(100);
+    expect(await page.data('isPause')).toBe(true);
   });
 
   it('test format', async () => {
-    page = await program.reLaunch('/pages/component/video/video-format');
+    page = await program.navigateTo('/pages/component/video/video-format');
     await page.waitFor(1000);
     expect(await page.data('isError')).toBe(false);
   });
