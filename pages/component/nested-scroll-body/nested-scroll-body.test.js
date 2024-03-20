4d2c365e1ad592d6c9bc4908db617fbe5bbdb449
shutao <shutao@dcloud.io>
2024-03-20 12:32:44 +0800

    新增nested-scroll-body、nested-scroll-header组件示例

---
+++ pages/component/nested-scroll-body/nested-scroll-body.test.js
@@ -0,0 +1,16 @@
+describe('component-native-nested-scroll-body', () => {
+  let page
+  beforeAll(async () => {
+    //打开lnested-scroll-body测试页
+    page = await program.reLaunch('/pages/component/nested-scroll-body/nested-scroll-body')
+    await page.waitFor(600)
+  })
+
+  //检测横向scroll_into_view属性赋值
+  it('check_scroll_into_view_left', async () => {
+    await page.callMethod('testBodyScrollBy', 400)
+    await page.waitFor(300)
+    const image = await program.screenshot();
+    expect(image).toMatchImageSnapshot();
+  })
+})
---
+++ pages/component/nested-scroll-header/nested-scroll-header.test.js
@@ -0,0 +1,15 @@
+describe('component-native-nested-scroll-header', () => {
+  let page
+  beforeAll(async () => {
+    //打开lnested-scroll-header测试页
+    page = await program.reLaunch('/pages/component/nested-scroll-header/nested-scroll-header')
+    await page.waitFor(600)
+  })
+
+
+  it('check_nested-scroll-header', async () => {
+    const image = await program.screenshot();
+    expect(image).toMatchImageSnapshot();
+  })
+})
+
