15b9ab2ca7819f2f3a2cbf7be58f9a2944de050a
unknown <wangyaqi988@163.com>
2024-04-20 12:10:53 +0800

    test: calendar页面不支持web端

--- pages/template/calendar/calendar.test.js
+++ pages/template/calendar/calendar.test.js
@@ -1,6 +1,12 @@
 const PAGE_PATH = '/pages/template/calendar/calendar'
 
 describe('calendar', () => {
+  if (process.env.uniTestPlatformInfo.indexOf('web') > -1 || process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true') {
+    it('dummyTest', () => {
+      expect(1).toBe(1)
+    })
+    return
+  }
   let page
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
