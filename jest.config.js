e30f5edd6a28591238d6f977f621511a34c9b83e
wan201809 <wandali@dcloud.io>
2024-04-23 15:44:56 +0800

    update jest.config.js 放开ios之前屏蔽的用例
    (cherry picked from commit 5cc27b912df47590ede2620d969d76d2e252a090)

--- jest.config.js
+++ jest.config.js
@@ -15,9 +15,6 @@ module.exports = {
   testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
   testPathIgnorePatterns: [
       '/node_modules/',
-      '<rootDir>/pages/API/download-file/download-file.test.js',
-      '<rootDir>/pages/API/upload-file/upload-file.test.js',
-      '<rootDir>/pages/API/get-battery-info/get-battery-info.test.js',
       '<rootDir>/pages/webview-screenshot-comparison/webview-screenshot-comparison.test.js',
       '<rootDir>/pages/webview-screenshot/webview-screenshot.test.js'
     ],
