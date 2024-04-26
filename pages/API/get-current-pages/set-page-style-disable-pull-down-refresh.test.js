2d3aed782b1dee82b1e922a039a0a22616d0e168
王亚琪 <wangyaqi988@163.com>
2024-04-26 13:08:41 +0800

    test: iOS端屏蔽不支持的测试例

--- pages/API/get-current-pages/set-page-style-disable-pull-down-refresh.test.js
+++ pages/API/get-current-pages/set-page-style-disable-pull-down-refresh.test.js
@@ -1,6 +1,12 @@
 const PAGE_PATH = '/pages/API/get-current-pages/set-page-style-disable-pull-down-refresh'
 
 describe('getCurrentPages', () => {
+  if (process.env.uniTestPlatformInfo.startsWith('IOS')) {
+    it('not-support', async () => {
+      expect(1).toBe(1)
+    })
+    return
+  }
   let page
   it('page-style', async () => {
     page = await program.navigateTo(PAGE_PATH)
@@ -18,7 +24,9 @@ describe('getCurrentPages', () => {
     expect(isEnablePullDownRefresh2).toBe(true)
     await page.callMethod('startPullDownRefresh')
     await page.waitFor(500)
-    const image3 = await program.screenshot({fullPage: true});
+    const image3 = await program.screenshot({
+      fullPage: true
+    });
     expect(image3).toSaveImageSnapshot();
 
     await page.waitFor(3500)
@@ -28,7 +36,9 @@ describe('getCurrentPages', () => {
     await page.waitFor(200)
     await page.callMethod('startPullDownRefresh')
     await page.waitFor(500)
-    const image2 = await program.screenshot({fullPage: true});
+    const image2 = await program.screenshot({
+      fullPage: true
+    });
     expect(image2).toSaveImageSnapshot();
 
   })
