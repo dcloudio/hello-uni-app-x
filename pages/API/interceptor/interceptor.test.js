14cffd7449c976f9b62fcad870f61987dbe13fdd
jixinbao <jixinbao@dcloud.io>
2024-05-06 11:43:40 +0800

    test(api/interceptor): 新增拦截器测试用例

--- pages/API/interceptor/interceptor.test.js
+++ pages/API/interceptor/interceptor.test.js
@@ -13,6 +13,14 @@ describe('interceptor', () => {
     const num = (await newPage.data()).page
     await program.navigateBack()
     expect(num).toBe(1)
+    // 新增 navigator 元素
+    const elementNavigatorButton = await page.$('.navigatorButton')
+    await elementNavigatorButton.tap()
+    await page.waitFor(300)
+
+    const currentPage = await program.currentPage()
+    expect(currentPage.path).toBe('pages/API/interceptor/page1')
+    await program.navigateBack()
   })
 
   it('addInterceptor', async () => {
@@ -22,6 +30,14 @@ describe('interceptor', () => {
     const num = (await newPage.data()).page
     await program.navigateBack()
     expect(num).toBe(2)
+    // 新增 navigator 元素
+    const elementNavigatorButton = await page.$('.navigatorButton')
+    await elementNavigatorButton.tap()
+    await page.waitFor(300)
+
+    const currentPage = await program.currentPage()
+    expect(currentPage.path).toBe('pages/API/interceptor/page2')
+    await program.navigateBack()
   })
 
   it('removeInterceptor', async () => {
@@ -31,5 +47,13 @@ describe('interceptor', () => {
     const num = (await newPage.data()).page
     await program.navigateBack()
     expect(num).toBe(1)
+    // 新增 navigator 元素
+    const elementNavigatorButton = await page.$('.navigatorButton')
+    await elementNavigatorButton.tap()
+    await page.waitFor(300)
+
+    const currentPage = await program.currentPage()
+    expect(currentPage.path).toBe('pages/API/interceptor/page1')
+    await program.navigateBack()
   })
 })
