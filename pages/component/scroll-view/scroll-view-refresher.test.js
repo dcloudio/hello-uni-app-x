a89cc23ee9996670964a455e242943345812e77b
Anne_LXM <liuxiaomei@dcloud.io>
2024-06-05 23:30:52 +0800

    新增默认下拉刷新事件自动化测试例

--- pages/component/scroll-view/scroll-view-refresher.test.js
+++ pages/component/scroll-view/scroll-view-refresher.test.js
@@ -13,6 +13,14 @@ describe('component-native-scroll-view-refresher', () => {
     await page.waitFor(300);
   });
 
+  async function eventCheck(dataName,typeName){
+    const eventInfo = await page.data(dataName)
+    expect(eventInfo.type).toBe(typeName)
+    expect(eventInfo.currentTarget).not.toBeFalsy();
+    expect(eventInfo.target).not.toBeFalsy();
+    expect(eventInfo.detail.dy).toBeGreaterThan(0)
+  }
+
   it('scroll-view-refresher-screenshot', async () => {
     //禁止滚动条
     await page.setData({
@@ -23,12 +31,19 @@ describe('component-native-scroll-view-refresher', () => {
     expect(image).toSaveImageSnapshot();
   })
 
-  it('check_refresher', async () => {
+  it('check_refresher_refresh_event', async () => {
     await page.setData({
       refresherTriggered: true
     })
     await page.waitFor(2000);
     expect(await page.data('refresherrefreshTimes')).toBe(1)
+    // 手动设置下拉刷新状态refresher-triggered为true时，在web和iOS不触发@refresherpulling事件
+    if(process.env.uniTestPlatformInfo.startsWith('android')){
+      await eventCheck('onRefresherpullingTest','refresherpulling')
+    }
+    await eventCheck('refresherrefreshTest','refresherrefresh')
+    await page.waitFor(1000);
+    await eventCheck('onRefresherrestoreTest','refresherrestore')
   });
 
   it('check_refresher_snapshot', async () => {
