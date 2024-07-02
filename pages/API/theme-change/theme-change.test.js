b21c4e69c4ec88a883037badc927baa82280bcdd
shutao <shutao@dcloud.io>
2024-07-02 18:38:07 +0800

    update theme-change.uvue 新增原始主题用于测试还原主题样式

--- pages/API/theme-change/theme-change.test.js
+++ pages/API/theme-change/theme-change.test.js
@@ -20,11 +20,13 @@ describe('API-theme-change', () => {
 
 
   it("check-set-app-theme", async () => {
+    const originalTheme = await page.data('originalTheme')
+    console.log("originalTheme是", originalTheme)
     await page.callMethod('setAppTheme', "dark")
     await page.waitFor(300)
     expect(await page.data('appTheme')).toBe("dark")
     //还原主题为light
-    await page.callMethod('setAppTheme', "light")
+    await page.callMethod('setAppTheme', originalTheme)
     await page.waitFor(600)
   })
 });
