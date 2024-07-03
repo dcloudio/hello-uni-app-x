65c59af08ff3fa38a2a4d40ca6c4d83d51c9c6c9
hdx <hdx@dcloud.io>
2024-07-03 10:50:34 +0800

    feat(canvas): 增加 toBlob/toDataURL 测试

---
+++ pages/component/canvas/canvas.test.js
@@ -0,0 +1,20 @@
+let page
+
+beforeAll(async () => {
+  page = await program.reLaunch('/pages/component/canvas/canvas')
+  await page.waitFor(2000)
+})
+
+describe('Canvas.uvue', () => {
+  it('toBlob', async () => {
+    if (process.env.uniTestPlatformInfo.toLowerCase().startsWith('web')) {
+      const {
+        testToBlobResult,
+        testToDataURLResult
+      } = await page.data()
+
+      expect(testToBlobResult).toBe(true)
+      expect(testToDataURLResult).toBe(true)
+    }
+  })
+})
