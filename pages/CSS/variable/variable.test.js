fdf4364cfc542bcada9750b1a6101fb207bbef47
zhenyuWang <13641039885@163.com>
2024-07-03 16:56:03 +0800

    test(variable): 规避 web-view 模式截图

--- pages/CSS/variable/variable.test.js
+++ pages/CSS/variable/variable.test.js
@@ -1,4 +1,10 @@
 describe('css-variable', () => {
+  if (process.env.UNI_AUTOMATOR_APP_WEBVIEW) {
+    it('app 与 web 存在差异, webview 不进行截图', () => {
+      expect(1).toBe(1)
+      return
+    })
+  }
   let page;
   beforeAll(async () => {
     page = await program.reLaunch('/pages/CSS/variable/variable');
