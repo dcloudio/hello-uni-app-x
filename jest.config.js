87dfd70dab9c59a8eb1c2dfb9fef6851b04ad368
zhenyuWang <13641039885@163.com>
2024-07-03 16:44:37 +0800

    test: 通过限制测试顺序，规避 pullDownRefresh 测试手势操作被弹框影响问题

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
-// 自动化测试
-
-// 备注：
-//
-// 1. testPathIgnorePatterns 忽略/pages/API的几条用例，是因为在ios设备上，运行会导致app崩溃。后期完成后，再去除。
-// 2. testPathIgnorePatterns 忽略webview相关用例, 是因为采用app-webview方式后，不需要这两个用例。请勿修改和提交到Git。
-//
+const path = require('path')
 
 module.exports = {
   testTimeout: 30000,
@@ -15,8 +9,7 @@ module.exports = {
   testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
   testPathIgnorePatterns: [
       '/node_modules/',
-      '<rootDir>/pages/webview-screenshot-comparison/webview-screenshot-comparison.test.js',
-      '<rootDir>/pages/webview-screenshot/webview-screenshot.test.js'
     ],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  testSequencer: path.join(__dirname, "testSequencer.js")
 }
