57c5bc5f25f6acc5d910181d08b729f78fdef8df
zhenyuWang <13641039885@163.com>
2024-07-03 16:44:37 +0800

    test: 通过限制测试顺序，规避 pullDownRefresh 测试手势操作被弹框影响问题

---
+++ testSequencer.js
@@ -0,0 +1,15 @@
+const Sequencer = require("@jest/test-sequencer").default
+const sortTestFilePaths = ["/pages/API/pull-down-refresh/pull-down-refresh.test.js"]
+class CustomSequencer extends Sequencer {
+  sort(tests) {
+    // 测试例排序
+    const sortedTests = sortTestFilePaths
+      .map((filePath) => {
+        return tests.find((test) => test.path.endsWith(filePath))
+      })
+      .filter(Boolean)
+    return [...new Set([...sortedTests, ...tests])]
+  }
+}
+
+module.exports = CustomSequencer
