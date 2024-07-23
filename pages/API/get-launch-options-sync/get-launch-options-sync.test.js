57d750cf78ecc16b45e20945793d8d6fa2a1d8aa
jixinbao <jixinbao@dcloud.io>
2024-07-23 19:20:31 +0800

    fix: 修复安卓平台类型告警

--- pages/API/get-launch-options-sync/get-launch-options-sync.test.js
+++ pages/API/get-launch-options-sync/get-launch-options-sync.test.js
@@ -10,25 +10,20 @@ describe('getLaunchOptionsSync', () => {
   })
   it('app onLaunch 和 getLaunchOptionsSync 结果一致', async () => {
 
-    if (process.env.uniTestPlatformInfo.toLowerCase().startsWith('android')) {
-      // if android return
-      expect(1).toBe(1)
-    }
-
     const page = await program.navigateTo(PAGE_PATH)
     await page.waitFor('view')
     const res = await page.callMethod('compareOnLaunchRes')
     expect(res.appOnLaunch).toEqual(res.launchOptions)
   })
   it('app onShow 和 getEnterOptionsSync 结果一致', async () => {
-    if (process.env.uniTestPlatformInfo.toLowerCase().startsWith('android')) {
-      // if android return
-      expect(1).toBe(1)
-    }
-
     const page = await program.navigateTo(PAGE_PATH)
     await page.waitFor('view')
     const res = await page.callMethod('compareOnShowRes')
+
+    if (process.env.uniTestPlatformInfo.toLowerCase().startsWith('android')) {
+      // if android return
+      expect(true).toBe(true)
+    }
     expect(res.appOnShow).toEqual(res.onShowOption)
   })
 })
