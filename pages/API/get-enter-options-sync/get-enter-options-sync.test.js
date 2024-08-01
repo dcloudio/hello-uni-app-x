21e5f84352daee21f713537ae72bac53308f90d3
hdx <hdx@dcloud.io>
2024-08-01 16:37:38 +0800

    getEnterOptionsSync: 简化示例，移除一些不必要的逻辑

--- pages/API/get-enter-options-sync/get-enter-options-sync.test.js
+++ pages/API/get-enter-options-sync/get-enter-options-sync.test.js
@@ -1,23 +1,10 @@
 const PAGE_PATH = '/pages/API/get-enter-options-sync/get-enter-options-sync'
 
 describe('getEnterOptionsSync', () => {
-  it('getEnterOptionsSync', async () => {
-    const page = await program.navigateTo(PAGE_PATH)
-    await page.waitFor('view')
-    await page.callMethod('getEnterOptionsSync')
-    const data = await page.data()
-    expect(data.checked).toBe(true)
-  })
- 
   it('app onShow 和 getEnterOptionsSync 结果一致', async () => {
     const page = await program.navigateTo(PAGE_PATH)
     await page.waitFor('view')
-    const res = await page.callMethod('compareOnShowRes')
-
-    if (process.env.uniTestPlatformInfo.toLowerCase().startsWith('android')) {
-      // if android return
-      expect(true).toBe(true)
-    }
-    expect(res.appOnShow).toEqual(res.onShowOption)
+    const pageData = await page.data()
+    expect(pageData.testResult).toBe(true)
   })
 })
