ce06da0b340e68345beb8477140abb08fd0f1052
hdx <hdx@dcloud.io>
2024-08-01 19:30:07 +0800

    feat(requestAnimationFrame/cancelAnimationFrame): 新增示例和测试, 暂不添加入口

---
+++ pages/API/requestAnimationFrame/requestAnimationFrame.test.js
@@ -0,0 +1,16 @@
+const PAGE_PATH = '/pages/API/requestAnimationFrame/requestAnimationFrame'
+
+describe('API-requestAnimationFrame', () => {
+  let page
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor(600);
+  });
+
+  it('requestAnimationFrame', async () => {
+    await page.callMethod('startRequestAnimationFrame')
+    await page.waitFor(100)
+    const data = await page.data()
+    expect(data.testFrameCount > 0).toBe(true)
+  });
+});
