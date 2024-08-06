7113f80e17eb219419e2c2b1f291d5007ba9385b
hdx <hdx@dcloud.io>
2024-08-06 17:33:06 +0800

    cavnas: 优化结构减少一些重复的逻辑

--- pages/component/canvas/canvas.test.js
+++ pages/component/canvas/canvas.test.js
@@ -25,22 +25,21 @@ describe('Canvas.uvue', () => {
   })
   it("测试异步创建canvas上下文", async () => {
     await page.callMethod('useAsync');
-    const element = await page.$('#testCanvasContext')
-    expect(await element.text()).toBe('true')
-
-  })
-  it("测试同步创建canvas上下文", async () => {
-    await page.callMethod('useAsync');
-    const element = await page.$('#testCanvasContext')
+    // const element = await page.$('#testCanvasContext')
     expect(await element.text()).toBe('true')
   })
+  // it("测试同步创建canvas上下文", async () => {
+  //   await page.callMethod('useAsync');
+  //   const element = await page.$('#testCanvasContext')
+  //   expect(await element.text()).toBe('true')
+  // })
   it('测试 canvasToDataURL', async () => {
     await page.callMethod('canvasToDataURL');
     const element = await page.$('#testToDataURLResult')
     expect(await element.text()).toBe('true')
   })
   it('测试 createImage', async () => {
-    await page.callMethod('drawImageAPI');
+    await page.callMethod('onCreateImage');
     await page.waitFor(500) // 加载图片
     const element = await page.$('#testCreateImage')
     expect(await element.text()).toBe('true')
