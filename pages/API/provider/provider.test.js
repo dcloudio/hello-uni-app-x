2f23ce812fa6ad060296d1d702d4271e45c3d462
mahaifeng <mahaifeng@dcloud.io>
2024-08-09 12:19:22 +0800

    [provider]补充provider测试用例

--- pages/API/provider/provider.test.js
+++ pages/API/provider/provider.test.js
@@ -21,5 +21,12 @@ describe("provider", () => {
     expect(providerIds[1]).toEqual('alipay')
     expect(providerIds[2]).toEqual('system')
     expect(providerIds[3]).toEqual('tencent')
+
+    let providerObjects = await page.data('providerObjects')
+    expect(providerObjects[0].description).toEqual('微信支付')
+    expect(providerObjects[1].description).toEqual('支付宝')
+    expect(providerObjects[2].description).toEqual('系统定位')
+    expect(providerObjects[3].description).toEqual('腾讯定位')
+    expect(providerObjects.length).toEqual(4)
   });
 });
