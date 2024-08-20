67a9abbcf4b33fca471c1d5022b168816f94f86a
XHY <xionghuayu@dcloud.io>
2024-08-20 16:35:49 +0800

    添加 rich-text.test 示例，测试动态修改内容组件高度是否正确

---
+++ pages/component/rich-text/rich-text.test.js
@@ -0,0 +1,32 @@
+const PAGE_PATH = '/pages/component/rich-text/rich-text'
+
+describe('rich-text-test', () => {
+
+  // 先屏蔽 android 及 web 平台
+  if (process.env.uniTestPlatformInfo.startsWith('android') || process.env.uniTestPlatformInfo.startsWith('web')) {
+    it('other platform', () => {
+      expect(1).toBe(1)
+    })
+    return
+  }
+
+  let page
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor(1000);
+  })
+
+
+  it('richt-text-height', async () => {
+    let beforeValue = await page.data('richTextHeight')
+    await page.callMethod('changeText')
+    await page.waitFor(500)
+    await page.callMethod('changeText')
+    await page.waitFor(500)
+    let afterValue = await page.data('richTextHeight')
+    console.log('beforeValue:', beforeValue)
+    console.log('afterValue:', afterValue)
+    expect(beforeValue).toBe(afterValue)
+  })
+
+})
