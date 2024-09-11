d7389302992b88ef503340b307959be24609a683
zhenyuWang <13641039885@163.com>
2024-09-11 17:48:23 +0800

    refactor(getApp): 补充 UniApp 示例及测试

--- pages/API/get-app/get-app.test.js
+++ pages/API/get-app/get-app.test.js
@@ -47,4 +47,12 @@ describe('getApp', () => {
     expect(newLifeCycleNum - oldLifeCycleNum).toBe(100)
     await page.callMethod('setLifeCycleNum', oldLifeCycleNum)
   })
+  it('eventBus', async () => {
+    const res = await page.callMethod('checkEventBus')
+    expect(res).toBe(true)
+  })
+  it('getAndroidApplication', async () => {
+    const res = await page.callMethod('checkGetAndroidApplication')
+    expect(res).toBe(true)
+  })
 })
