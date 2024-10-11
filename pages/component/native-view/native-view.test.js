f4f2d2ac8cdcc85460db88855665cfd500acc76e
zhenyuWang <13641039885@163.com>
2024-10-11 15:06:07 +0800

    fix: 修复分支合并错误

---
+++ pages/component/native-view/native-view.test.js
@@ -0,0 +1,17 @@
+describe('native-view.uvue', () => {
+  if (process.env.uniTestPlatformInfo.indexOf('web') > -1 || process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true') {
+    it('object', () => {
+      expect(1).toBe(1)
+    })
+    return
+  }
+  beforeAll(async () => {
+    page = await program.reLaunch('/pages/component/native-view/native-view')
+  });
+
+  it('native-view检测init函数是否响应', async () => {
+    await page.waitFor(600)
+    const value = await page.data('isLoad')
+    expect(value).toBe(true)
+  })
+})
