6baa7a433195520f05e66f55fb66863ffb5023e2
zhenyuWang <13641039885@163.com>
2024-12-07 18:49:10 +0800

    test(downloadFile): 优化测试逻辑

--- pages/API/download-file/download-file.test.js
+++ pages/API/download-file/download-file.test.js
@@ -4,17 +4,28 @@ describe('ExtApi-DownloadFile', () => {
 
   let page;
   let res;
+  let timeout = 3000
+  let waitForStartTime
+  async function waitCallbackTriggredOrTimeout(){
+    waitForStartTime = Date.now()
+    await page.waitFor(async () => {
+      const callbackTriggred = await page.data('jest_callback_triggred')
+      return callbackTriggred || (Date.now() - waitForStartTime > timeout)
+    })
+  }
+
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
-    await page.waitFor(600);
+    await page.waitFor('view');
     await page.callMethod('jest_downloadFile');
-    await page.waitFor(2000);
+    await waitCallbackTriggredOrTimeout()
     res = await page.data('jest_result');
   });
 
   beforeEach(async () => {
     await page.setData({
-      jest_result: false
+      jest_result: false,
+      jest_callback_triggred: false
     })
   });
 
@@ -24,7 +35,7 @@ describe('ExtApi-DownloadFile', () => {
 
   it('Check Special characters Url download file', async () => {
     res = await page.callMethod('jest_special_characters_download')
-    await page.waitFor(2000);
+    await waitCallbackTriggredOrTimeout()
     res = await page.data('jest_result');
     expect(res).toBe(true)
   });
@@ -35,7 +46,7 @@ describe('ExtApi-DownloadFile', () => {
   ) {
     it('Check uni.env', async () => {
       await page.callMethod('jest_downloadFile_with_uni_env');
-      await page.waitFor(2000);
+      await waitCallbackTriggredOrTimeout()
       res = await page.data('jest_result');
       expect(res).toBe(true);
     });
@@ -47,7 +58,7 @@ describe('ExtApi-DownloadFile', () => {
     if (!process.env.uniTestPlatformInfo.toLocaleLowerCase().startsWith('ios') || version > 15) {
       it('Check Download File In UTS Module', async () => {
         res = await page.callMethod('jest_uts_module_invoked')
-        await page.waitFor(2000);
+        await waitCallbackTriggredOrTimeout()
         res = await page.data('jest_result');
         expect(res).toBe(true)
       })
@@ -70,13 +81,13 @@ describe('ExtApi-DownloadFile', () => {
 
   it('Check Set Cookie', async () => {
     res = await page.callMethod('jest_set_cookie')
-    await page.waitFor(2000);
+    await waitCallbackTriggredOrTimeout()
     res = await page.data('jest_result');
     expect(res).toBe(true)
   });
   it('Check Delete Cookie', async () => {
     res = await page.callMethod('jest_delete_cookie')
-    await page.waitFor(2000);
+    await waitCallbackTriggredOrTimeout()
     res = await page.data('jest_result');
     expect(res).toBe(true)
   });
