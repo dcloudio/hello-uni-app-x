fe7835627844c8ed250e6178929a06ebf4f782ea
anne-xml <liuxiaomei@dcloud.io>
2024-12-10 17:56:17 +0800

    更新element-get-attribute兼容app和web

--- pages/API/element-get-attribute/element-get-attribute.test.js
+++ pages/API/element-get-attribute/element-get-attribute.test.js
@@ -1,12 +1,7 @@
 jest.setTimeout(30000);
-describe('/pages/API/element-get-attribute/element-get-attribute', () => {
-  let page;
-  if (!process.env.uniTestPlatformInfo.startsWith('mp')) {
-    it('skip', () => {
-      expect(1).toBe(1)
-    })
-    return
-  }
+describe('test element-get-attribute', () => {
+  let page,platform,attrStyle,propertyValue;
+  platform = process.env.UNI_PLATFORM
   beforeAll(async () => {
     page = await program.reLaunch('/pages/API/element-get-attribute/element-get-attribute')
     await page.waitFor(3000);
@@ -17,13 +12,23 @@ describe('/pages/API/element-get-attribute/element-get-attribute', () => {
   });
   it('check setStyle getAttributeStyle', async () => {
     await page.callMethod('setStyle')
-    await page.callMethod('getAttributeStyle')
-    expect(await page.data('attrStyle')).toEqual('background-color:#FFF000');
+    if(platform != 'app-plus'){
+      await page.callMethod('getAttributeStyle')
+      attrStyle = platform == 'mp-weixin'?'background-color:#FFF000;':'padding: 0.625rem; background-color: rgb(255, 240, 0);'
+      expect(await page.data('attrStyle')).toEqual(attrStyle);
+    }
   });
-  it('check scrollTo', async () => {
-    await page.callMethod('scrollTo')
-    await page.waitFor(100);
-    const scrollView =  await page.$('.scroll-view_H')
-    expect(await scrollView.property('scrollLeft')).toBe(200);
+  it('check getPropertyValue', async () => {
+    await page.callMethod('getPropertyValue')
+    propertyValue = platform == 'h5'?'rgb(255, 240, 0)':'#FFF000'
+    expect(await page.data('propertyValue')).toEqual(propertyValue);
   });
+  if(platform != 'h5'){
+    it('check scrollTo', async () => {
+      await page.callMethod('scrollTo')
+      await page.waitFor(100);
+      const scrollView =  await page.$('.scroll-view_H')
+      expect(await scrollView.property('scrollLeft')).toBe(200);
+    });
+  }
 });
