43ed6ed585187d1cf8db1bfe0aad85b16a46f87b
anne-xml <liuxiaomei@dcloud.io>
2025-01-09 16:22:54 +0800

    test(element-get-attribute):调整测试例

--- pages/API/element-get-attribute/element-get-attribute.test.js
+++ pages/API/element-get-attribute/element-get-attribute.test.js
@@ -29,6 +29,24 @@ describe('test element-get-attribute', () => {
     const propertyValue = isWeb?'rgb(255, 240, 0)':'#FFF000'
     expect(await page.data('propertyValue')).toEqual(propertyValue);
   });
+
+  it('getBoundingClientRectSync', async () => {
+    await page.callMethod("getBoundingClientRectAsyncChild");
+    await page.waitFor(100)
+    const rectInfo = await page.data("rectInfo")
+    const systemInfo = await program.systemInfo();
+    const width = systemInfo.screenWidth
+    console.log('width: ',width);
+    console.log('rectInfo: ',rectInfo);
+    expect(Math.round(rectInfo.x)).toBe(15)
+    expect(Math.round(rectInfo.y) >= 242).toBe(true)
+    expect(width - 15 * 2 - Math.round(rectInfo.width) >= 0).toBe(true)
+    expect(Math.round(rectInfo.height)).toBe(100)
+    expect(Math.round(rectInfo.left)).toBe(15)
+    expect(Math.round(rectInfo.top) >= 242).toBe(true)
+    expect(width - 15 - Math.round(rectInfo.right) >= 0).toBe(true)
+    expect(Math.round(rectInfo.bottom) > 342).toBe(true)
+  })
   if(isApp||isMP){
     it('check scrollTo', async () => {
       await page.callMethod('scrollTo')
