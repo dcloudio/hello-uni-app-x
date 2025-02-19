7a8e824415479899280d88f9a03db53d5892688b
zhenyuWang <13641039885@163.com>
2025-02-19 17:10:52 +0800

    feat(cover-view): 支持 app 端

--- pages/component/cover-view/cover-view.test.js
+++ pages/component/cover-view/cover-view.test.js
@@ -1,16 +1,16 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isAndroid = platformInfo.startsWith('android')
+const isIos = platformInfo.startsWith('ios')
+const isApp = isAndroid || isIos
+
 let page;
 describe('web-cover-view', () => {
-  if (!process.env.uniTestPlatformInfo.startsWith('web')) {
-    it('app', () => {
-      expect(1).toBe(1)
-    })
-    return
-  }
   beforeAll(async () => {
     page = await program.reLaunch('/pages/component/cover-view/cover-view')
     await page.waitFor('view');
     await page.waitFor('cover-view');
-    await page.waitFor('cover-image');
+    // app 端 cover-image 会被转换为 image
+    await page.waitFor(isApp ? 'image' : 'cover-image');
     await page.waitFor('map');
     // 等待地图加载完成
     const waitTime = process.env.uniTestPlatformInfo.includes('firefox') ? 5000:3000
@@ -18,7 +18,13 @@ describe('web-cover-view', () => {
   });
 
   it('screenshot', async () => {
-    expect(await program.screenshot()).toSaveImageSnapshot();
+    const image = await program.screenshot({
+      deviceShot: true,
+      area: {
+        x: 0,
+        y: 200,
+      }
+    });
+    expect(image).toSaveImageSnapshot();
   });
-
 });
