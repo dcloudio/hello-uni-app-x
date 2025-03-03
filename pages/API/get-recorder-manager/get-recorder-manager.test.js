25db197479c6ec94017fb2972a99eb7d3efb68a3
mahaifeng <mahaifeng@dcloud.io>
2025-03-03 18:09:00 +0800

    [recorder] 修改示例与添加测试用例

---
+++ pages/API/get-recorder-manager/get-recorder-manager.test.js
@@ -0,0 +1,50 @@
+describe('recorder', () => {
+  if (!(process.env.uniTestPlatformInfo.startsWith('android'))) {
+    it('app', () => {
+      expect(1).toBe(1)
+    })
+    return
+  }
+
+  beforeAll(async () => {
+    page = await program.reLaunch('/pages/API/get-recorder-manager/get-recorder-manager')
+    await page.waitFor(600);
+  });
+  it('onError', async () => {
+    await page.waitFor(100)
+    const btnError = await page.$('#btn-error')
+    await btnError.tap()
+    await page.waitFor(200)
+    expect(await page.data('registerError')).toBeTruthy()
+  });
+  it('start and onStart', async () => {
+    await page.waitFor(1000)
+    const btnStart = await page.$('#btn-startRecord')
+    await btnStart.tap()
+    await page.waitFor(200)
+    expect(await page.data('recording')).toBeTruthy()
+  })
+
+  it('onStop', async () => {
+    await page.waitFor(1000)
+    const btnStop = await page.$('#btn-stopRecord')
+    await btnStop.tap()
+    await page.waitFor(200)
+    expect(await page.data('recording')).toBeFalsy()
+  });
+  it('startPlay', async () => {
+    await page.waitFor(100)
+    const startPlay = await page.$('#btn-startPlay')
+    await startPlay.tap()
+    await page.waitFor(200)
+    expect(await page.data('playing')).toBeTruthy()
+  });
+
+  it('stopPlay', async () => {
+    await page.waitFor(100)
+    const stopPlay = await page.$('#btn-stopPlay')
+    await stopPlay.tap()
+    await page.waitFor(200)
+    expect(await page.data('playing')).toBeFalsy()
+  });
+});
