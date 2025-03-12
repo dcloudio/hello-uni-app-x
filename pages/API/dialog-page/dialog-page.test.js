64e21fce1fa125d37c51f02beacfcc4103457ac2
zhenyuWang <13641039885@163.com>
2025-03-12 09:04:48 +0800

    test: 规避 harmony 测试超时

--- pages/API/dialog-page/dialog-page.test.js
+++ pages/API/dialog-page/dialog-page.test.js
@@ -5,11 +5,14 @@ const isWeb = platformInfo.startsWith('web')
 const isAndroid = platformInfo.startsWith('android')
 const isIos = platformInfo.startsWith('ios')
 const isMP = platformInfo.startsWith('mp')
+const isHarmony = platformInfo.startsWith('harmony')
+
 const FIRST_PAGE_PATH = '/pages/API/dialog-page/dialog-page'
 const NEXT_PAGE_PATH = '/pages/API/dialog-page/next-page'
 
 describe('dialog page', () => {
-  if (process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true' || isMP) {
+  // harmony 进入该页面，会导致后续测试超时
+  if (process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true' || isMP || isHarmony) {
     it('not support', () => {
       expect(1).toBe(1)
     })
