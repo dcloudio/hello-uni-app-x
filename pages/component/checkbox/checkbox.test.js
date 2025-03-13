7a1e00aa8ab7e400b1e9b5f985933000feebf607
hdx <hdx@dcloud.io>
2025-03-13 18:13:12 +0800

    test: 回退 radio/checkbox

--- pages/component/checkbox/checkbox.test.js
+++ pages/component/checkbox/checkbox.test.js
@@ -16,26 +16,24 @@ beforeAll(async () => {
 describe('Checkbox.uvue', () => {
   const isMP = process.env.uniTestPlatformInfo.startsWith('mp')
   const isHarmony = platformInfo.startsWith('harmony')
-  if (!isHarmony) {
-    it('change', async () => {
-      expect(await getData('value')).toEqual([])
-      const cb1 = await page.$('.cb1')
-      await cb1.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual(['cb', 'cb1'])
-      const cb = await page.$('.cb')
-      await cb.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual(['cb1'])
-      const cb2 = await page.$('.cb2')
-      await cb2.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual(['cb1'])
-      await cb1.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual([])
-    })
-  }
+  it('change', async () => {
+    expect(await getData('value')).toEqual([])
+    const cb1 = await page.$('.cb1')
+    await cb1.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual(['cb', 'cb1'])
+    const cb = await page.$('.cb')
+    await cb.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual(['cb1'])
+    const cb2 = await page.$('.cb2')
+    await cb2.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual(['cb1'])
+    await cb1.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual([])
+  })
   it('length', async () => {
     const checkboxGroupElements = await page.$$('.checkbox-group')
     expect(checkboxGroupElements.length).toBe(4)
@@ -114,14 +112,12 @@ describe('Checkbox.uvue', () => {
       })
       expect(await cb.attribute('foreColor')).toBe('#63acfe')
     })
-    if (!isHarmony) {
-      it('trigger UniCheckboxGroupChangeEvent', async () => {
-        const element = await page.$('.checkbox-item-0')
-        await element.tap()
-        await page.waitFor(1000)
-        const { testEvent } = await page.data()
-        expect(testEvent).toBe(true)
-      })
-    }
+    it('trigger UniCheckboxGroupChangeEvent', async () => {
+      const element = await page.$('.checkbox-item-0')
+      await element.tap()
+      await page.waitFor(1000)
+      const { testEvent } = await page.data()
+      expect(testEvent).toBe(true)
+    })
   }
 })
--- pages/component/radio/radio.test.js
+++ pages/component/radio/radio.test.js
@@ -16,23 +16,21 @@ describe('Radio.uvue', () => {
   const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
   const isMP = platformInfo.startsWith('mp')
   const isHarmony = platformInfo.startsWith('harmony')
-  if (!isHarmony) {
-    it('change', async () => {
-      expect(await getData('value')).toEqual('')
-      const radio1 = await page.$('.r1')
-      await radio1.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual('r1')
-      const radio = await page.$('.r')
-      await radio.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual('r')
-      const radio2 = await page.$('.r2')
-      await radio2.tap()
-      await page.waitFor(100)
-      expect(await getData('value')).toEqual('r')
-    })
-  }
+  it('change', async () => {
+    expect(await getData('value')).toEqual('')
+    const radio1 = await page.$('.r1')
+    await radio1.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual('r1')
+    const radio = await page.$('.r')
+    await radio.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual('r')
+    const radio2 = await page.$('.r2')
+    await radio2.tap()
+    await page.waitFor(100)
+    expect(await getData('value')).toEqual('r')
+  })
   it('length', async () => {
     const radioGroupElements = await page.$$('.radio-group')
     expect(radioGroupElements.length).toBe(4)
@@ -88,7 +86,7 @@ describe('Radio.uvue', () => {
       expect(await radio.attribute('disabled')).toBe(false + '')
     })
   }
-  if(!isMP && !isHarmony) {
+  if(!isMP) {
     it('trigger UniRadioGroupChangeEvent', async () => {
       const { current } = await page.data()
 
