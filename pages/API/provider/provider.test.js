60bc782fe8fedc628bcfd920dd0d76b6f50c79ec
zhaofengliang <zhaofengliang@dcloud.io>
2025-03-20 15:42:55 +0800

    fix： 调整 provider 示例，解决自动化测试失败。

--- pages/API/provider/provider.test.js
+++ pages/API/provider/provider.test.js
@@ -13,6 +13,7 @@ describe("provider", () => {
   	return
   }
 
+
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor(600)
@@ -28,10 +29,10 @@ describe("provider", () => {
     expect(providerIds[3]).toEqual('tencent')
 
     let providerObjects = await page.data('providerObjects')
-    expect(providerObjects[0].description).toEqual('微信支付')
-    expect(providerObjects[1].description).toEqual('支付宝')
-    expect(providerObjects[2].description).toEqual('系统定位')
-    expect(providerObjects[3].description).toEqual('腾讯定位')
+    expect(providerObjects[0]).toEqual('微信支付')
+    expect(providerObjects[1]).toEqual('支付宝')
+    expect(providerObjects[2]).toEqual('系统定位')
+    expect(providerObjects[3]).toEqual('腾讯定位')
     expect(providerObjects.length).toEqual(4)
   });
 });
