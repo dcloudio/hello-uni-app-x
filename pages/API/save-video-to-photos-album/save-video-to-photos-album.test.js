91dc80b2b9f297e6c293ed7435a9a9d0b108d7a2
zhenyuWang <13641039885@163.com>
2025-03-24 17:39:58 +0800

    test: 鸿蒙平台放开部分测试例

--- pages/API/save-video-to-photos-album/save-video-to-photos-album.test.js
+++ pages/API/save-video-to-photos-album/save-video-to-photos-album.test.js
@@ -2,10 +2,11 @@ const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
 const isIOS = platformInfo.startsWith('ios')
 const isMP = platformInfo.startsWith('mp')
 const isWeb = platformInfo.startsWith('web')
+const isAndroid = platformInfo.startsWith('android')
 const isHarmony = platformInfo.startsWith('harmony')
 
 describe('API-saveVideoToPhotosAlbum', () => {
-  if (isIOS || isWeb || isMP || isHarmony) {
+  if (isIOS || isWeb || isMP) {
     it('pass', async () => {
       expect(1).toBe(1);
     });
@@ -19,12 +20,16 @@ describe('API-saveVideoToPhotosAlbum', () => {
   });
 
   it('test saveVideoToPhotosAlbum', async () => {
-    if (process.env.uniTestPlatformInfo.startsWith('android')) {
+    if (isAndroid) {
       await program.adbCommand(
         'pm grant io.dcloud.uniappx android.permission.WRITE_EXTERNAL_STORAGE');
-      await page.waitFor(500);
     }
     await page.callMethod('saveVideo');
+    if (isHarmony) {
+      await program.tap({x: 305, y: 568})
+    }
+    await page.waitFor(500);
     expect(await page.data('success')).toBe(true);
+    await page.waitFor(2000);
   });
 });
