dea031b152377a17d00b4d4961b9863a833b5942
zhenyuWang <13641039885@163.com>
2025-04-03 15:36:42 +0800

    test: fix 定位元素单独截图

--- pages/CSS/layout/z-index.test.js
+++ pages/CSS/layout/z-index.test.js
@@ -1,11 +1,14 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isAndroid = platformInfo.startsWith('android')
+
 describe('css-z-index', () => {
   let page;
   beforeAll(async () => {
     page = await program.reLaunch('/pages/CSS/layout/z-index');
   });
 
-  it('test crash', async () => {
-    if (process.env.uniTestPlatformInfo.startsWith('android')) {
+  if (isAndroid) {
+    it('test crash', async () => {
       await page.setData({
         autoTest: true
       });
@@ -14,8 +17,18 @@ describe('css-z-index', () => {
       await page.setData({
         autoTest: false
       });
-    } else {
-      expect(1).toBe(1);
-    }
+    });
+  }
+
+  it('screenshot', async () => {
+    const windowInfo = await program.callUniMethod('getWindowInfo');
+    const image = await program.screenshot({
+      deviceShot: true,
+      area: {
+        x: 0,
+        y: windowInfo.safeAreaInsets.top + 44
+      }
+    });
+    expect(image).toSaveImageSnapshot();
   });
 });
