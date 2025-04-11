dd4dc04ec21594ff6de4b77a281c25e2af46b37b
zhenyuWang <13641039885@163.com>
2025-04-11 16:36:41 +0800

    test: 优化测试代码

--- pages/API/save-image-to-photos-album/save-image-to-photos-album.test.js
+++ pages/API/save-image-to-photos-album/save-image-to-photos-album.test.js
@@ -22,14 +22,12 @@ describe('API-saveImageToPhotosAlbum', () => {
 
   it('test saveImageToPhotosAlbum', async () => {
     if (isAndroid) {
-      if (process.env.uniTestPlatformInfo.startsWith('android')) {
-             const infos = process.env.uniTestPlatformInfo.split(' ');
-             const version = parseInt(infos[infos.length - 1]);
-             if (version < 24) {
-               console.log("安卓版本小于24设备 不进行saveImage测试，模拟器会出现闪退影响后续测试")
-               expect(1).toBe(1)
-               return
-             }
+      const infos = platformInfo.split(' ');
+      const version = parseInt(infos[infos.length - 1]);
+      if (version < 24) {
+        console.log("安卓版本小于24设备 不进行saveImage测试，模拟器会出现闪退影响后续测试")
+        expect(1).toBe(1)
+        return
       }
       await program.adbCommand(
         'pm grant io.dcloud.uniappx android.permission.WRITE_EXTERNAL_STORAGE');
