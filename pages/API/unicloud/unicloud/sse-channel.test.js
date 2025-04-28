302bf19f2b53aa6e2278da7bb03c48887d2d733d
王亚琪 <wangyaqi988@163.com>
2025-04-28 11:49:14 +0800

    test: 修复测试例错误

--- pages/API/unicloud/unicloud/sse-channel.test.js
+++ pages/API/unicloud/unicloud/sse-channel.test.js
@@ -2,8 +2,13 @@ const PAGE_PATH = '/pages/API/unicloud/unicloud/sse-channel'
 const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
 
 describe('unicloud-database', () => {
-  it('databaseLookup', async () => {
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+  })
+
+  it('sse channel', async () => {
     await page.callMethod('receiveMessage')
+    await page.waitFor(2000)
     const {
       messages
     } = await page.data()
