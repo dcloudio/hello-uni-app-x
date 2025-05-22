5bb88caf195ee0dca3ecac1f8f83868e0e0c47fd
yinjiacheng <yinjiacheng@dcloud.io>
2025-05-22 18:09:11 +0800

    更新web-view示例和自动化测试用例

--- pages/component/web-view/web-view.test.js
+++ pages/component/web-view/web-view.test.js
@@ -182,4 +182,11 @@ describe('component-native-web-view', () => {
     const has = await page.callMethod('checkNativeWebView')
     expect(has).toBe(true)
   })
+
+  it('test lodaData', async () => {
+    await page.callMethod('loadData');
+    await page.waitFor(1000);
+    const image = await program.screenshot({ fullPage: true });
+    expect(image).toSaveImageSnapshot();
+  });
 });
