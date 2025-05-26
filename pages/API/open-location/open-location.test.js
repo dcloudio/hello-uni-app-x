840131c9ae4d08cb9ce2d4361f65a9295ae5f35b
wangzhenyu66 <wangzhenfa@dcloud.io>
2025-05-26 18:55:03 +0800

    test(openLocation): 精简测试

--- pages/API/open-location/open-location.test.js
+++ pages/API/open-location/open-location.test.js
@@ -28,32 +28,11 @@ describe('dialog page', () => {
 		await page.waitFor(1000)
   });
 
-  it('dialogPage should empty', async () => {
+  it('dialogPage should be opened', async () => {
 		const dialogPagesNum = await page.data('dialogPagesNum')
-		expect(dialogPagesNum).toBe(0)
+		expect(dialogPagesNum).toBe(1)
 	})
-
-	it('should trigger parent hide', async () => {
-		const lifecycleNum = await page.callMethod('getLifeCycleNum')
-		expect(lifecycleNum).toBe(originLifeCycleNum - 1)
-	})
-	if (isIos) {
-		// 15以下的模拟器所对应的xcode不能编译自定义插件，大于15是因为某台设备，会用xcode14.1跑15.5的设备
-    let version = process.env.uniTestPlatformInfo
-    let split = version.split(" ")
-		version = parseInt(split[split.length - 1])
-		if (version > 15) {
-			it('call openLocation in uts plugin', async () => {
-				page = await program.reLaunch('/pages/API/open-location/open-location')
-				await page.waitFor('view');
-				await page.callMethod('openLocationByPlugin')
-				await page.waitFor(1000)
-				const lifecycleNum = await page.callMethod('getLifeCycleNum')
-				expect(lifecycleNum).toBe(originLifeCycleNum - 1)
-			})
-		}
-	}
-
+	
 	afterAll(async () => {
 		await page.callMethod('setLifeCycleNum', originLifeCycleNum)
   });
