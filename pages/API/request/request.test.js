36783f2300f3646a1ae8ac9e2f975f97ecd9bf00
zhaofengliang <zhaofengliang@dcloud.io>
2025-05-28 15:27:11 +0800

    feat: RequestTask 示例移除条件编译

--- pages/API/request/request.test.js
+++ pages/API/request/request.test.js
@@ -206,64 +206,62 @@ describe('ExtApi-Request', () => {
     })
   }
 
-  if (isIOS) {
-    it('Check RequestTask related api', async () => {
-      const onHeadersReceived_observe_1_btn = await page.$('#onHeadersReceived_observe_1')
-      await onHeadersReceived_observe_1_btn.tap()
 
-      const onHeadersReceived_observe_2_btn = await page.$('#onHeadersReceived_observe_2')
-      await onHeadersReceived_observe_2_btn.tap()
+  it('Check RequestTask related api', async () => {
+    const onHeadersReceived_observe_1_btn = await page.$('#onHeadersReceived_observe_1')
+    await onHeadersReceived_observe_1_btn.tap()
 
-
-      const onChunkReceived_observe_1_btn = await page.$('#onChunkReceived_observe_1')
-      await onChunkReceived_observe_1_btn.tap()
-
-      const onChunkReceived_observe_2_btn = await page.$('#onChunkReceived_observe_2')
-      await onChunkReceived_observe_2_btn.tap()
+    const onHeadersReceived_observe_2_btn = await page.$('#onHeadersReceived_observe_2')
+    await onHeadersReceived_observe_2_btn.tap()
 
 
-      const checkRequestTask_btn = await page.$('#checkRequestTask')
+    const onChunkReceived_observe_1_btn = await page.$('#onChunkReceived_observe_1')
+    await onChunkReceived_observe_1_btn.tap()
+
+    const onChunkReceived_observe_2_btn = await page.$('#onChunkReceived_observe_2')
+    await onChunkReceived_observe_2_btn.tap()
+
+
+    const checkRequestTask_btn = await page.$('#checkRequestTask')
+    await checkRequestTask_btn.tap()
+
+
+    await page.waitFor(5000);
+    let jest_requestTask_result = await page.data('jest_requestTask_result')
+    if (jest_requestTask_result) {
+      let res = await page.data('res')
+      let checkSuccess = res.includes('onHeadersReceived监听1') && res.includes('onHeadersReceived监听2') && res
+        .includes('onChunkReceived监听1') && res.includes('onChunkReceived监听2')
+      expect(checkSuccess).toBe(true)
+
+      const offHeadersReceived_id_btn = await page.$('#offHeadersReceived_id')
+      await offHeadersReceived_id_btn.tap()
+
       await checkRequestTask_btn.tap()
 
+      await page.waitFor(5000);
+      jest_requestTask_result = await page.data('jest_requestTask_result')
+      if (jest_requestTask_result) {
+        res = await page.data('res')
+        checkSuccess = res.includes('onHeadersReceived监听2') == false && res.includes('onHeadersReceived监听1')
+        expect(checkSuccess).toBe(true)
+      }
+
+      const offChunkReceived_observe_btn = await page.$('#offChunkReceived_observe')
+      await offChunkReceived_observe_btn.tap()
+      const offHeadersReceived_observe_btn = await page.$('#offHeadersReceived_observe')
+      await offHeadersReceived_observe_btn.tap()
+
+      await checkRequestTask_btn.tap()
 
       await page.waitFor(5000);
-      let jest_requestTask_result = await page.data('jest_requestTask_result')
+      jest_requestTask_result = await page.data('jest_requestTask_result')
       if (jest_requestTask_result) {
-        let res = await page.data('res')
-        let checkSuccess = res.includes('onHeadersReceived监听1') && res.includes('onHeadersReceived监听2') && res
-          .includes('onChunkReceived监听1') && res.includes('onChunkReceived监听2')
+        res = await page.data('res')
+        checkSuccess = res.includes('点击了 offHeadersReceived') && res.includes('点击了 offChunkReceived')
         expect(checkSuccess).toBe(true)
-
-        const offHeadersReceived_id_btn = await page.$('#offHeadersReceived_id')
-        await offHeadersReceived_id_btn.tap()
-
-        await checkRequestTask_btn.tap()
-
-        await page.waitFor(5000);
-        jest_requestTask_result = await page.data('jest_requestTask_result')
-        if (jest_requestTask_result) {
-          res = await page.data('res')
-          checkSuccess = res.includes('onHeadersReceived监听2') == false && res.includes('onHeadersReceived监听1')
-          expect(checkSuccess).toBe(true)
-        }
-
-        const offChunkReceived_observe_btn = await page.$('#offChunkReceived_observe')
-        await offChunkReceived_observe_btn.tap()
-        const offHeadersReceived_observe_btn = await page.$('#offHeadersReceived_observe')
-        await offHeadersReceived_observe_btn.tap()
-
-        await checkRequestTask_btn.tap()
-
-        await page.waitFor(5000);
-        jest_requestTask_result = await page.data('jest_requestTask_result')
-        if (jest_requestTask_result) {
-          res = await page.data('res')
-          checkSuccess = res.includes('点击了 offHeadersReceived') && res.includes('点击了 offChunkReceived')
-          expect(checkSuccess).toBe(true)
-        }
       }
-    })
-
-  }
+    }
+  })
 
 });
