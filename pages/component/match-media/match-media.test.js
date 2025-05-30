a16d564485b8a8d85a115f63017366873f37ec3c
姬彬彬 <jibinbin@dcloud.io>
2025-05-30 15:29:01 +0800

    fix: 修复match-media组件截图错误的问题

--- pages/component/match-media/match-media.test.js
+++ pages/component/match-media/match-media.test.js
@@ -1,7 +1,11 @@
 describe("match-media", () => {
+    let page;
+    beforeAll(async () => {
+      page = await program.reLaunch('/pages/component/match-media/match-media')
+      await page.waitFor(2000);
+    });
+
   it("match-media screenshot", async () => {
-    page = await program.currentPage();
-    await page.waitFor(3000);
     const image = await program.screenshot({
       fullPage: true,
     });
