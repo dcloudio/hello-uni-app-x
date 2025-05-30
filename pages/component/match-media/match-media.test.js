857755a7e4b886babbbe259ad177455cbfb133d5
姬彬彬 <jibinbin@dcloud.io>
2025-05-30 14:58:51 +0800

    feat: 添加match-media示例

---
+++ pages/component/match-media/match-media.test.js
@@ -0,0 +1,10 @@
+describe("match-media", () => {
+  it("match-media screenshot", async () => {
+    page = await program.currentPage();
+    await page.waitFor(3000);
+    const image = await program.screenshot({
+      fullPage: true,
+    });
+    expect(image).toSaveImageSnapshot();
+  });
+});
