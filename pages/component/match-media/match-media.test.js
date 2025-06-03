ee3daf732f21a7ca1e21bf92d074036dfaf9a023
姬彬彬 <jibinbin@dcloud.io>
2025-06-03 11:48:19 +0800

    test: match-media组件限制测试平台

--- pages/component/match-media/match-media.test.js
+++ pages/component/match-media/match-media.test.js
@@ -1,4 +1,17 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isMP = platformInfo.startsWith('mp')
+const isHarmony = platformInfo.toLocaleLowerCase().startsWith('harmony')
+const isAndroid = platformInfo.startsWith('android')
+
 describe("match-media", () => {
+    const isSupportted = isMP || isHarmony || isAndroid
+    if (!isSupportted) {
+      it("not support", () => {
+        expect(1).toBe(1);
+      });
+      return;
+    }
+    
     let page;
     beforeAll(async () => {
       page = await program.reLaunch('/pages/component/match-media/match-media')
