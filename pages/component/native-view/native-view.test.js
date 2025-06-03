7433c370b70a9bbed6d1b1f1ce43b06bfbdd061f
wangzhenyu66 <wangzhenfa@dcloud.io>
2025-06-03 20:14:53 +0800

    fix(native-view): 修复逻辑错误

--- pages/component/native-view/native-view.test.js
+++ pages/component/native-view/native-view.test.js
@@ -17,7 +17,7 @@ describe('native-view.uvue', () => {
     if(
       platformInfo.indexOf('14.5') != -1 ||
       platformInfo.indexOf('13.7') != -1 ||
-      platformInfo.indexOf('12.4') != -1``
+      platformInfo.indexOf('12.4') != -1
       ){
         // TODO: 排查 ios 不兼容版本 测试异常原因
         it('14.5 13.7 12.4 测试异常', () => {
