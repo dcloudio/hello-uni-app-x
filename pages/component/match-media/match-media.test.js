00036315aa4e224d91d184ff94ba7bed62ccb3bb
姬彬彬 <jibinbin@dcloud.io>
2025-06-09 11:10:48 +0800

    style: 格式化代码

--- pages/component/match-media/match-media.test.js
+++ pages/component/match-media/match-media.test.js
@@ -4,19 +4,19 @@ const isAndroid = platformInfo.startsWith('android')
 const isHarmony = platformInfo.startsWith('harmony')
 
 describe('match-media', () => {
-    const isSupportted = isMP || isAndroid || isHarmony;
-    if (!isSupportted) {
-      it('not support', () => {
-        expect(1).toBe(1);
-      });
-      return;
-    }
-    
-    let page;
-    beforeAll(async () => {
-      page = await program.reLaunch('/pages/component/match-media/match-media')
-      await page.waitFor(2000);
+  const isSupportted = isMP || isAndroid || isHarmony;
+  if (!isSupportted) {
+    it('not support', () => {
+      expect(1).toBe(1);
     });
+    return;
+  }
+
+  let page;
+  beforeAll(async () => {
+    page = await program.reLaunch('/pages/component/match-media/match-media')
+    await page.waitFor(2000);
+  });
 
   it('match-media screenshot', async () => {
     const image = await program.screenshot({
