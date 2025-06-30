729c87121ec3a89ca716e65ca9484ef682c3e144
wangzhenyu66 <wangzhenfa@dcloud.io>
2025-06-30 14:58:27 +0800

    test(loadFontFace): 修复测试例错误

--- pages/API/load-font-face/load-font-face.test.js
+++ pages/API/load-font-face/load-font-face.test.js
@@ -2,8 +2,9 @@ const PAGE_PATH = '/pages/API/load-font-face/load-font-face'
 const CHILD_PAGE_PATH = "/pages/API/load-font-face/load-font-face-child";
 
 describe("loadFontFace", () => {
+  let page
   beforeAll(async () => {
-    const page = await program.navigateTo(PAGE_PATH);
+    page = await program.navigateTo(PAGE_PATH);
     await page.waitFor(4000);
   });
   it("check callback triggered", async () => {
