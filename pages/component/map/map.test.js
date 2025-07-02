da93dbf0af50fad455e9f7028d15a3945904112e
wangzhenyu66 <wangzhenfa@dcloud.io>
2025-07-02 16:08:49 +0800

    refactor: 代码优化

--- pages/component/map/map.test.js
+++ pages/component/map/map.test.js
@@ -31,10 +31,8 @@ describe('web-map', () => {
 
     it('Check EventDetail JsonStringify', async () => {
       const res = await page.data('jestResult')
-      console.log(res.eventDetailJsonStringify);
       expect(res.eventDetailJsonStringify).not.toBe("{}");
     })
-
     return
   }
 
@@ -88,6 +86,5 @@ describe('web-map', () => {
     const scaleRes = await page.data('jestResult')
     expect(scaleRes.scale).toBeGreaterThanOrEqual(5);
     expect(scaleRes.scale).toBeLessThanOrEqual(18);
-    console.log("jestResult",await page.data())
   });
 });
