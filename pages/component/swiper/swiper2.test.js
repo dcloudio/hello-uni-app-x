0453a488d7d72689c94d3c5df20fbf99b6d21410
wangzhenyu66 <wangzhenfa@dcloud.io>
2025-07-03 19:39:00 +0800

    test: 修复 harmony 自动化测试失败问题

--- pages/component/swiper/swiper2.test.js
+++ pages/component/swiper/swiper2.test.js
@@ -27,18 +27,16 @@ describe('swiper-touch-test', () => {
   it('滑动切换 swiper', async () => {
     let x = await page.data('swipeX')
     let y = await page.data('swipeY')
-    // harmony onReady getBoundingClientRect 获取节点宽度错误
-    if (isHarmony && x < 20) {
-      x = 300
-    }
     // program.swipe Android10以上不生效
     if(isAndroid && res.osAndroidAPILevel > 28){
-    }else{
-      await program.swipe({
-        startPoint: {x, y},
-        endPoint: {x: 10, y},
-        duration: 200
-      })
+      // 避免 android 10 侧滑影响
+      x -= 20
+    }
+    await program.swipe({
+      startPoint: {x, y},
+      endPoint: {x: 10, y},
+      duration: 200
+    })
 
       await page.waitFor(1000)
       let val = await page.data('currentValChange')
