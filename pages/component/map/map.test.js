a5c561617e4d67c0835e3fe16335258d3e933d88
anne-xml <liuxiaomei@dcloud.io>
2025-07-18 19:55:59 +0800

    fix(map): 修复小程序端某些情况下地图组件初始化时显示错误位置的问题

--- pages/component/map/map.test.js
+++ pages/component/map/map.test.js
@@ -25,7 +25,7 @@ describe('web-map', () => {
 
   if (isApp) {
     it('handleMoveToLocation', async () => {
-      await page.callMethod('handleMoveToLocation')
+      await page.callMethod('handleMoveToLocation',true)
       await page.waitFor(500);
       const moveToLocationRes = await page.data('jestResult')
       expect(moveToLocationRes.moveToLocationMsg).toBe("moveToLocation:ok");
