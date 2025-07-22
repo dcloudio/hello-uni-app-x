2ef6542d0fcf16941716cf9a0d97869330968236
wangzhenyu66 <wangzhenfa@dcloud.io>
2025-07-22 20:02:10 +0800

    test(slider-100): 优化截图测试

--- pages/pages.test.js
+++ pages/pages.test.js
@@ -281,9 +281,10 @@ const pages = [
   // 依赖 onload 参数获取 web-view src
   // '/uni_modules/uni-pay-x/pages/ad-interactive-webview/ad-interactive-webview',
   // '/uni_modules/uni-pay-x/pages/pay-desk/pay-desk',
-  '/pages/template/custom-long-list/custom-long-list',
-  '/pages/template/test-background-color-content/test-background-color-content',
-  '/pages/template/slider-100/slider-100',
+  // 页面内容不稳定
+  // '/pages/template/custom-long-list/custom-long-list',
+  // 单独测试例截图
+  // '/pages/template/slider-100/slider-100',
   // 动态内容
   // '/pages/template/long-list-nested/long-list-nested',
 ]
