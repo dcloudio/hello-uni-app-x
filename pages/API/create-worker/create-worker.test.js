2c010cbb9262d86973551270bd26b61968d222ce
DCloud_LXH <283700113@qq.com>
2025-08-28 14:11:14 +0800

    fix(test-create-worker): 修复 create-worker.test.js 没有坚听 onMessage

--- pages/API/create-worker/create-worker.test.js
+++ pages/API/create-worker/create-worker.test.js
@@ -8,7 +8,7 @@ const isAndroid = platformInfo.startsWith('android')
 const isHarmony = platformInfo.startsWith('harmony')
 
 describe('Api-createWorker', () => {
-  if(isMP || isWeb || isHarmony || isIOS) {
+  if(isIOS) {
     it('skip', async () => {
       expect(1).toBe(1)
     })
@@ -20,16 +20,19 @@ describe('Api-createWorker', () => {
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor(500);
+  });
+
+  it('workerTask ', async () => {
     await page.callMethod('test_resetInputValue');
     await page.waitFor(500);
     await page.callMethod('create');
     await page.waitFor(500);
+    await page.callMethod('onWorkerMsg');
     await page.callMethod('sendMessage');
-    await page.waitFor(500);
-    res = await page.data('taskResult');
-  });
-
-  it('workerTask ', async () => {
-    expect(res).toBe('2');
+    await page.waitFor(async () => {
+      const taskResult = await page.data('taskResult')
+      return taskResult.length > 0
+    });
+    expect(await page.data('taskResult')).toBe('2');
   });
 });
