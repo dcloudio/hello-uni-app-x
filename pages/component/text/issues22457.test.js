fbe9edaa7a3b29660f506de5724c2473a225b42b
XHY <xionghuayu@dcloud.io>
2025-10-16 17:40:35 +0800

    新增 issues22457 测试例

---
+++ pages/component/text/issues22457.test.js
@@ -0,0 +1,18 @@
+const PAGE_PATH = '/pages/component/text/issues22457'
+
+describe('issues-22457', () => {
+  const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+
+  let page
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor(500);
+  })
+
+  it('absolute-text-width', async () => {
+
+    let textWidth = await page.data('textWidth')
+    // textWidth 应该大于 20
+    expect(textWidth).toBeGreaterThan(20)
+  })
+})
