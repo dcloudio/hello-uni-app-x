0d740ccc6f685c367b1e222ab29b51090e7368e3
anne-xml <liuxiaomei@dcloud.io>
2025-12-29 19:37:01 +0800

    test(style-isolation): 修复测试例path错误

--- pages/CSS/style-isolation/style-isolation.test.js
+++ pages/CSS/style-isolation/style-isolation.test.js
@@ -7,16 +7,9 @@ const isWeb = platformInfo.startsWith('web')
 const isMP = platformInfo.startsWith('mp')
 const isAppWebView = process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true'
 
-const PAGE_PATH = '/pages/css/style-isolation/style-isolation'
+const PAGE_PATH = '/pages/CSS/style-isolation/style-isolation'
 
 describe('style-isolation', () => {
-  // 此测试例暂时只对鸿蒙开放
-  if(!isHarmony) {
-    it('skip', () => {
-      expect(1).toBe(1)
-    })
-    return
-  }
 
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
@@ -26,14 +19,14 @@ describe('style-isolation', () => {
   it('测试组件的根节点为二级组件时 样式传递', async () => {
     const levelEl = await page.$('.level-child-class')
 		console.log('levelEl',levelEl)
-		
+
 		const levelChildEl = await levelEl.$('.box')
 		console.log('levelChildEl',levelChildEl)
-		
+
 		const levelChildStyle = await levelChildEl.style('background-color')
 		console.log('levelChildStyle',levelChildStyle)
-		
+
 		expect(levelChildStyle).toBe('#00aaff!important')
-		
+
   })
 });
