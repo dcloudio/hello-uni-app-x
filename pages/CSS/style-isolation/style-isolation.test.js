350164bfe310ce1b152f3b3d8919d3b20abfa358
anne-xml <161556142+anne-xml@users.noreply.github.com>
2026-01-19 21:18:14 +0800

    feat(style-isolation):新增样式隔离styleIsolation配置测试例

--- pages/CSS/style-isolation/style-isolation.test.js
+++ pages/CSS/style-isolation/style-isolation.test.js
@@ -1,7 +1,11 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isHarmony = platformInfo.startsWith('harmony')
+const isWeb = platformInfo.startsWith('web')
+const isDom2 = process.env.UNI_APP_X_DOM2 === "true"
 const PAGE_PATH = '/pages/CSS/style-isolation/style-isolation'
 
 describe('style-isolation', () => {
-  if (process.env.UNI_APP_X_DOM2 !== 'true') {
+  if (!(isWeb || (isDom2 && isHarmony))) {
     it('skip: DOM1 不支持', () => {
       expect(1).toBe(1)
     })
@@ -15,12 +19,74 @@ describe('style-isolation', () => {
 
   it('测试组件的根节点为二级组件时 样式传递', async () => {
     const levelEl = await page.$('.level-child-class')
-		console.log('levelEl',levelEl)
-
 		const levelElStyle = await levelEl.style('background-color')
-		console.log('levelElStyle',levelElStyle)
-
-		expect(levelElStyle).toBe('rgb(0, 170, 255)')
-
+    if(isHarmony){
+      expect(levelElStyle).toBe('#00aaffff')
+    }
+    if(isWeb){
+      expect(levelElStyle).toBe('rgb(0, 170, 255)')
+    }
   })
+
+  it('styleIsolation-isolated模式 - 全隔离', async () => {
+    const compIsolatedEl = await page.$('.comp-isolated')
+    // 样式全隔离，预期组件自身样式紫色
+    const comBoxEl = await compIsolatedEl.$('.com-box')
+    const comBoxStyle = await comBoxEl.style('background-color')
+    if(isHarmony){
+      expect(comBoxStyle).toBe('#d9d1ffff')
+    }
+    if(isWeb){
+      expect(comBoxStyle).toBe('rgb(217, 209, 255)')
+    }
+    // 验证全局样式无效，预期组件默认字体大小16px
+    const globalTestEl = await compIsolatedEl.$('.global-text')
+    const globalTestStyle = await globalTestEl.style('font-size')
+    expect(globalTestStyle).toBe('16px')
+  })
+
+  it('styleIsolation-app模式 - 受全局样式影响', async () => {
+    const compAppEl = await page.$('.comp-app')
+    // 优先级：全局样式 < 自身样式，预期组件自身样式紫色
+    const comBoxEl = await compAppEl.$('.com-box')
+    const comBoxStyle = await comBoxEl.style('background-color')
+    if(isHarmony){
+      expect(comBoxStyle).toBe('#d9d1ffff')
+    }
+    if(isWeb){
+      expect(comBoxStyle).toBe('rgb(217, 209, 255)')
+    }
+    // 验证，全局样式有效(字体的粗细bold与 700 等值，大小18px)
+    const globalTestEl = await compAppEl.$('.global-text')
+    const globalTestWeight = await globalTestEl.style('font-weight')
+    const globalTestSize = await globalTestEl.style('font-size')
+    expect(globalTestWeight).toBe('700')
+    expect(globalTestSize).toBe('18px')
+    // 验证，页面样式无效，默认字体大小16px
+    const pageTestEl = await compAppEl.$('.page-text')
+    const pageTestSize = await pageTestEl.style('font-size')
+    expect(pageTestSize).toBe('16px')
+  })
+
+  it('styleIsolation-app-and-page模式 - 受全局和页面样式影响', async () => {
+    const compAppAndPageEl = await page.$('.comp-app-and-page')
+    // 优先级：全局样式 < 页面样式 < 自身样式，预期组件自身样式紫色
+    const comBoxEl = await compAppAndPageEl.$('.com-box')
+    const comBoxStyle = await comBoxEl.style('background-color')
+    if(isHarmony){
+      expect(comBoxStyle).toBe('#d9d1ffff')
+    }
+    if(isWeb){
+      expect(comBoxStyle).toBe('rgb(217, 209, 255)')
+    }
+    // 验证，全局样式有效(字体的粗细bold，大小18px)
+    const globalTestEl = await compAppAndPageEl.$('.global-text')
+    const globalTestSize = await globalTestEl.style('font-size')
+    expect(globalTestSize).toBe('18px')
+    // 验证，页面样式有效，字体大小14px
+    const pageTestEl = await compAppAndPageEl.$('.page-text')
+    const pageTestSize = await pageTestEl.style('font-size')
+    expect(pageTestSize).toBe('14px')
+  })
+
 });
