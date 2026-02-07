41b2e43b05d55b5b309abb54be2ca2711e1415db
zhanglei <zhanglei@dcloud.io>
2026-02-07 12:51:14 +0800

    fix:屏蔽微信小程序上图片预览的截图逻辑，删除无用注释。

--- pages/API/preview-image/preview-image-multi.test.js
+++ pages/API/preview-image/preview-image-multi.test.js
@@ -2,7 +2,8 @@
 describe('test-previewImage-multi', () => {
   const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
   const isWeb = platformInfo.startsWith('web')
-  if (isWeb) {
+  const isMP = platformInfo.startsWith('mp')
+  if (isWeb || isMP) {
     it('other platform', () => {
       expect(1).toBe(1)
     })
