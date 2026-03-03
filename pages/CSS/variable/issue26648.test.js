9370d5f2028ccd0c1c3ca2bafc422195ad62e7f9
xty <m0_73419315@noreply.gitcode.com>
2026-03-03 10:25:12 +0800

    refactor: 移除issue 26648 无用代码

--- pages/CSS/variable/issue26648.test.js
+++ pages/CSS/variable/issue26648.test.js
@@ -1,14 +1,6 @@
 // uni-app自动化测试教程: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/
 const isAppWebView = process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true'
 
-const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
-const isAndroid = platformInfo.startsWith('android')
-const isIOS = platformInfo.startsWith('ios')
-const isWeb = platformInfo.startsWith('web')
-const isHarmony = platformInfo.startsWith('harmony')
-
-const isApp = isAndroid || isIOS || isHarmony
-
 describe('/pages/CSS/variable/issue26648.uvue', () => {
   if (isAppWebView) {
     it('app 与 web 存在差异, webview 不进行截图', () => {
