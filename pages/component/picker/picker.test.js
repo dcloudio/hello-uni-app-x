edad39b598a39e9660787e2e0b782e2521f9d1e9
jibinbin <jibinbin@dcloud.io>
2026-03-18 19:28:07 +0800

    style(picker): 格式化测试用例代码

--- pages/component/picker/picker.test.js
+++ pages/component/picker/picker.test.js
@@ -25,7 +25,9 @@ describe('Picker.uvue', () => {
   }
 
   async function setPageData(newData) {
-    return await page.setData({ data: newData });
+    return await page.setData({
+      data: newData
+    });
   }
 
   function getValue(className, propertyValue = 'value') {
@@ -44,7 +46,9 @@ describe('Picker.uvue', () => {
 
   it('picker selector', async () => {
     expect(await getValue('.picker-selector--test')).toBe(await getData('index'))
-    await setPageData({index: 1})
+    await setPageData({
+      index: 1
+    })
     const pickerValueEl = await page.$('.picker-selector--value')
     expect(await pickerValueEl.text()).toBe('美国')
 
@@ -57,34 +61,44 @@ describe('Picker.uvue', () => {
     expect(await getValue('.picker-multi--test')).toEqual(await getData('multiIndex'))
     const pickerMultiValueEl = await page.$('.picker-multi--value')
     expect(await pickerMultiValueEl.text()).toStrictEqual('亚洲，中国，北京')
-    await setPageData({multiIndex: [0, 0, 2]})
+    await setPageData({
+      multiIndex: [0, 0, 2]
+    })
     const pickerMultiValueEl2 = await page.$('.picker-multi--value')
     expect(await pickerMultiValueEl2.text()).toStrictEqual('亚洲，中国，广州')
   })
 
   it('picker time', async () => {
     expect(await getValue('.picker-time--test')).toBe(await getData('time'))
-    await setPageData({time: '15:30'})
+    await setPageData({
+      time: '15:30'
+    })
     expect(await getValue('.picker-time--test')).toEqual('15:30')
   })
 
   it('picer date-day', async () => {
     expect(await getValue('.picker-date-day--test')).toBe(await getData('dayDate'))
-    await setPageData({dayDate: '2028-05-20'})
+    await setPageData({
+      dayDate: '2028-05-20'
+    })
     expect(await getValue('.picker-date-day--test')).toEqual('2028-05-20')
     expect(await getValue('.picker-date-day--test', 'start')).toStrictEqual(await getData('startDate'))
     expect(await getValue('.picker-date-day--test', 'end')).toStrictEqual(await getData('endDate'))
   })
   it('picker date-month', async () => {
     expect(await getValue('.picker-date-month--test')).toBe(await getData('monthDate'))
-    await setPageData({monthDate: '2028-05'})
+    await setPageData({
+      monthDate: '2028-05'
+    })
     expect(await getValue('.picker-date-month--test')).toEqual('2028-05')
     expect(await getValue('.picker-date-month--test', 'start')).toStrictEqual(await getData('startDate'))
     expect(await getValue('.picker-date-month--test', 'end')).toStrictEqual(await getData('endDate'))
   })
   it('picker date-year', async () => {
     expect(await getValue('.picker-date-year--test')).toBe(await getData('yearDate'))
-    await setPageData({yearDate: '2028'})
+    await setPageData({
+      yearDate: '2028'
+    })
     expect(await getValue('.picker-date-year--test')).toEqual('2028')
     expect(await getValue('.picker-date-year--test', 'start')).toStrictEqual(await getData('startDate'))
     expect(await getValue('.picker-date-year--test', 'end')).toStrictEqual(await getData('endDate'))
