25c7abe2eb5503627dae7a2a2bd29dcc13318ee1
dc-shutao <shutao@dcloud.io>
2026-04-02 18:39:33 +0800

    Revert "chore(scroll-view-props): 暂时屏蔽android检测 scroll_into_view 跳转孙子节点的检测"
    This reverts commit d58bbb238d5e494fdc15acd052016c3af8dae561.

--- pages/component/scroll-view/scroll-view-props.test.js
+++ pages/component/scroll-view/scroll-view-props.test.js
@@ -1,6 +1,5 @@
 const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
 const isMP = platformInfo.startsWith('mp')
-const isAndroid = platformInfo.startsWith('android')
 
 describe('component-native-scroll-view-props', () => {
   let page;
@@ -48,19 +47,16 @@ describe('component-native-scroll-view-props', () => {
       expect(scrollTop-570).toBeGreaterThanOrEqual(0)
   })
 
-  if(!isAndroid) {//Android scroll_into_view暂时未支持孙节点
-    //检测竖向scroll_into_view滚动到孙节点属性赋值
-    it('check_scroll_into_view_top_grand_child', async () => {
-        await setPageData({ scrollIntoView: "grand_child_item3" })
-        await page.waitFor(600)
-        const element = await page.$('#scrollViewY')
-        const scrollTop = await element.property("scrollTop")
-        console.log("check_scroll_into_view_top--"+scrollTop)
-        await setPageData({ scrollIntoView: "" })
-        expect(scrollTop-570).toBeGreaterThanOrEqual(0)
-    })
-  }
-
+  //检测竖向scroll_into_view滚动到孙节点属性赋值
+  it('check_scroll_into_view_top_grand_child', async () => {
+      await setPageData({ scrollIntoView: "grand_child_item3" })
+      await page.waitFor(600)
+      const element = await page.$('#scrollViewY')
+      const scrollTop = await element.property("scrollTop")
+      console.log("check_scroll_into_view_top--"+scrollTop)
+      await setPageData({ scrollIntoView: "" })
+      expect(scrollTop-570).toBeGreaterThanOrEqual(0)
+  })
 
   if(!isMP) {
     //检测横向可滚动区域
@@ -95,20 +91,17 @@ describe('component-native-scroll-view-props', () => {
     expect(scrollLeft-930).toBeGreaterThanOrEqual(0)
   })
 
-
-  if(!isAndroid) {//Android scroll_into_view 暂未支持孙节点
-    //检测横向scroll_into_view滚动到孙节点属性赋值
-    it('check_scroll_into_view_left_grand_child', async () => {
-      await setPageData({ scrollX: true })
-      await setPageData({ scrollIntoView: "horizontal_grand_child__item3" })
-      await page.waitFor(600)
-      const element = await page.$('#scrollViewX')
-      const scrollLeft = await element.property("scrollLeft")
-      console.log("check_scroll_into_view_left--"+scrollLeft)
-      await setPageData({ scrollIntoView: "" })
-      expect(scrollLeft-930).toBeGreaterThanOrEqual(0)
-    })
-  }
+  //检测横向scroll_into_view滚动到孙节点属性赋值
+  it('check_scroll_into_view_left_grand_child', async () => {
+    await setPageData({ scrollX: true })
+    await setPageData({ scrollIntoView: "horizontal_grand_child__item3" })
+    await page.waitFor(600)
+    const element = await page.$('#scrollViewX')
+    const scrollLeft = await element.property("scrollLeft")
+    console.log("check_scroll_into_view_left--"+scrollLeft)
+    await setPageData({ scrollIntoView: "" })
+    expect(scrollLeft-930).toBeGreaterThanOrEqual(0)
+  })
 
   //截图对比
   it('scroll-view-props-screenshot', async () => {
