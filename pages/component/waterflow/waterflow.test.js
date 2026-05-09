e3fa2ceb1b18f1068e78b3862c19455c9a7a6520
lizhongyi <lizhongyi@dcloud.io>
2026-05-09 19:49:59 +0800

    暂时屏蔽waterFlow在iOS上的自动化测试

--- pages/component/waterflow/waterflow.test.js
+++ pages/component/waterflow/waterflow.test.js
@@ -4,10 +4,11 @@ const isWeb = platformInfo.startsWith('web')
 const isHarmony = platformInfo.startsWith('harmony')
 const isAppWebView = process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true'
 const isDom2 = process.env.UNI_APP_X_DOM2 === "true"
+const isIOS = platformInfo.startsWith('ios')
 
 describe('component-native-waterflow', () => {
   // 鸿蒙平台api 20支持滚动相关事件，api 18支持load-more。目前先手动测试，后续升级测试机后再放开测试
-  if (isMP || isWeb || isAppWebView) {
+  if (isMP || isWeb || isAppWebView || isIOS) {
   	it('not support', () => {
   		expect(1).toBe(1)
   	})
