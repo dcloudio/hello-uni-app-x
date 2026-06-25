96f4da1fd0411072b2f2dcb4244e09745797ad76
wangzhenyu66 <wangzhenfa@dcloud.io>
2026-06-25 13:16:51 +0800

    test: onLoad navigateBack 测试用例放开非 android 平台

--- pages/API/navigator/new-page/onLoad.test.js
+++ pages/API/navigator/new-page/onLoad.test.js
@@ -82,16 +82,16 @@ describe("onLoad", () => {
     page = await program.currentPage();
     expect(page.path).toBe(TARGET_PAGE_PATH.substring(1));
   });
-  it("navigateBack", async () => {
-    if (isAndroid && !isAppWebView) {
-      page = await program.reLaunch(INTERMEDIATE_PAGE_PATH);
-      await page.waitFor('view');
-      await page.callMethod("navigateToOnLoadWithType", "navigateBack");
-      await page.waitFor('view');
-      page = await program.currentPage();
-      expect(page.path).toBe(INTERMEDIATE_PAGE_PATH.substring(1));
-    }
-  });
+  if (!isAppWebView) {
+    it("navigateBack", async () => {
+        page = await program.reLaunch(INTERMEDIATE_PAGE_PATH);
+        await page.waitFor('view');
+        await page.callMethod("navigateToOnLoadWithType", "navigateBack");
+        await page.waitFor(1000);
+        page = await program.currentPage();
+        expect(page.path).toBe(INTERMEDIATE_PAGE_PATH.substring(1));
+    });
+  }
   it("redirectTo", async () => {
     page = await program.reLaunch(INTERMEDIATE_PAGE_PATH);
     await page.waitFor('view');
