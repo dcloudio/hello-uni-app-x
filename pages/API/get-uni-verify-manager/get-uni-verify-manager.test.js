6c4096fb58995b6527dc1303216755dfc29c9e6a
DCloud_LXH <283700113@qq.com>
2026-08-05 10:49:02 +0800

    test(harmony): 添加 getUniVerifyManager 自动化测试

---
+++ pages/API/get-uni-verify-manager/get-uni-verify-manager.test.js
@@ -0,0 +1,36 @@
+const PAGE_PATH = '/pages/API/get-uni-verify-manager/get-uni-verify-manager'
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isHarmony = platformInfo.startsWith('harmony')
+const isDom2 = process.env.UNI_APP_X_DOM2 === 'true'
+
+describe('uni-verify', () => {
+  if (!isHarmony) {
+    it('skip', async () => {
+      expect(1).toBe(1);
+    });
+    return;
+  }
+
+  async function waitForData(path, matcher) {
+    const start = Date.now()
+    await page.waitFor(async () => {
+      const value = await page.data(path)
+      return matcher(value)
+    })
+  }
+
+  let page;
+  let res;
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor(1000)
+    await page.setData({
+      data: { autoTest: true}
+    })
+  });
+
+  it('customLogin', async () => {
+    await waitForData('data.autoTestPreLoginSuccess', value => value === true)
+    await waitForData('data.loginSuccess', value => value === true)
+  })
+});
